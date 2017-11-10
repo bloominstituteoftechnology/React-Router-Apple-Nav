138dd29fe869e97de5999545e57574799bfaf43e
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-11-10 10:26:25 -0700

    initial commit

---
+++ apple/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
