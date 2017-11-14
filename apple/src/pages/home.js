117c23d2c531be837566c37e107f2e7683bd59c4
Christian Franco <christian.franco94@icloud.com>
2017-11-14 01:52:49 -0800

    finished homework

--- apple/src/pages/home.js
+++ apple/src/pages/home.js
@@ -3,7 +3,6 @@ import Navigation from '../components/navigation'
 
 export default props => {
     const { match } = props;
-    console.log(match);
     return (
         <div className="home">
             <Navigation path={match.path}/>
