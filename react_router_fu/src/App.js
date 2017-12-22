ff8d12c3d4aae9bf541213edea9d5580b3b1a531
ubuntu <irishseanvaldivia@gmail.com>
2017-12-22 12:49:04 -0600

    Let's get this show rolling!

--- react_router_fu/src/App.js
+++ react_router_fu/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './pages/home';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/mac" component={Home}></Route>
+            <Route path="/ipad" component={Home}></Route>
+            <Route path="/iphone" component={Home}></Route>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
