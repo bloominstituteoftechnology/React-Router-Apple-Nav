ae3861d3619d111170285f6bc071a5cb92c5709c
Adam Fetters <adamfetterscoder@gmail.com>
2017-12-22 13:35:06 -0500

    setup server mostly

--- apple-mock/Server/server.js
+++ apple-mock/Server/server.js
@@ -12,9 +12,17 @@ server.use(cors(allowedClients));
 
 server.use(bodyParser.json());
 
-server.get('/api/subMenu/:menuName', (req, res) => {
-  res.status(200).json()
+const menus = [
+  {name: "Mac", menuItems: [
+    "MacBook", "MacBook Air", "MacBook Pro", "iMac", "iMac Pro", "Mac Pro", "Mac Mini", "Accessories", "High Sierra", "Compare"
+  ]}
+]
+
+server.get('/api/menu/:menuName', (req, res) => {
+  const menu = menus.filter(menu => menu.name === req.params.name)
+  res.status(200).send(menu);
 });
+
 
 server.listen(9000, () => {
   console.log('running on port 9000');
