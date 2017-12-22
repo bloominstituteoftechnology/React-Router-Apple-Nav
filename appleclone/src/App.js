431cdfc664f867ce136e7a7ec61fc81d09f04ce7
Jason Metten <metten@gmail.com>
2017-12-22 12:28:14 -0600

    React-Dom Skeleton for Future Use

--- appleclone/src/App.js
+++ appleclone/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+
+import { Home, Nav, subNav1, subNav2, subNav3, subNav4, subNav5 } from './components';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav />
+        <div className="subpage">
+          <Route exact={true} path="/" component={Home}/>
+          <Route path="/subNav1" component={subNav1} />
+          <Route path="/subNav2" component={subNav2} />
+          <Route path="/subNav3" component={subNav3} />
+          <Route path="/subNav4" component={subNav4} />
+          <Route path="/subNav5" component={subNav5} />
+        </div>
       </div>
     );
   }
---
+++ appleclone/src/components/index.js
@@ -0,0 +1,17 @@
+import Home from './Home';
+import Nav from './Nav';
+import subNav1 from './SubNav1';
+import subNav2 from './SubNav2';
+import subNav3 from './SubNav3';
+import subNav4 from './SubNav4';
+import subNav5 from './SubNav5';
+
+export {
+  Home,
+  Nav,
+  subNav1,
+  subNav2,
+  subNav3,
+  subNav4,
+  subNav5,
+}
