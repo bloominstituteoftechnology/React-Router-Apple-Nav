888755b6b6a4233c251f25527769236fa575722a
sarika <sarikaresume@gmail.com>
2017-12-22 09:51:01 -0800

    directory

---
+++ Answers.md
@@ -0,0 +1,4 @@
+1. What is the difference between _server side routing_ and _client side routing_?
+1. Mention some advantages of using client side routing.
+1. Which component is used to define a route and what _props_ are commonly added to it?
+1. How can I make sure that the component associated with the _"/"_ route is not displayed for every other route?
---
+++ nav.js
@@ -0,0 +1,19 @@
+<nav id="primary_nav">
+         
+    <ul>
+     
+        <li><a href="">Top</a></li>
+     
+        <li><a href="">Navigation</a></li>
+     
+        <li><a href="">Horizontal</a></li>
+     
+        <li><a href="">Bar</a></li>
+     
+        <li><a href="">Across</a></li>
+     
+        <li><a href="#home">Top</a></li>
+     
+    </ul>
+ 
+</nav><!--end primary_nav-->
