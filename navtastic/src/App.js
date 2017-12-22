070afb1647637a046f0568c287a95176c4aff372
Aaron Wilder <element1028@gmail.com>
2017-12-22 10:03:09 -0800

    Getting routes setup

--- navtastic/src/App.js
+++ navtastic/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <h1>Hello World</h1>
+          <ul>
+            <li><Link className="link" to="/About">About</Link></li>
+            <li><Link className="link" to="/Christmas">Christmas</Link></li>
+            <li><Link className="link" to="/happy">Happy</Link></li>
+          </ul>
+        </div>
+      </BrowserRouter>
     );
   }
 }
