c542c9c0aa48aad595de8f272e4f0e053cb6c7ef
Guelmis Cortina <cguelmis@yahoo.com>
2018-01-18 10:26:57 -0500

    add the submenus

--- pears/src/components/IPad.js
+++ pears/src/components/IPad.js
@@ -1,8 +1,60 @@
 import React from 'react';
+import { Route, NavLink } from 'react-router-dom';
 
-function IPad() {
+function IPadPro() {
 	return (
-		<h1>IPad Page</h1>
+		<div>
+			<h1>IPad Pro</h1>
+			<h3>Anything you can do, you can do better</h3>
+		</div>
+	);
+}
+
+function IPadV() {
+	return (
+		<div>
+			<h1>iPad</h1>
+			<h3>Flat-out fun.</h3>
+		</div>
+	);
+}
+
+function IPadMini() {
+	return (
+		<div>
+			<h1>iPad mini 4</h1>
+			<h3>Mighty.Small.</h3>
+		</div>
+	);
+}
+
+function IPad(props) {
+	const { match } = props;
+	return (
+		<div>
+			<h1>IPad Page</h1>
+			<ul>
+				<li className="navLink">
+					<NavLink to={`${match.url}/pro`} activeClassName="navlink--selected">
+						IPad Pro
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to={`${match.url}/ipad-9.7`} activeClassName="navlink--selected">
+						IPad
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to={`${match.url}/ipad-mini-4`} activeClassName="navlink--selected">
+						IPad Mini
+					</NavLink>
+				</li>
+			</ul>
+
+			<Route path={`${match.path}/pro`} component={IPadPro} />
+			<Route path={`${match.path}/ipad-9.7`} component={IPadV} />
+			<Route path={`${match.path}/ipad-mini-4`} component={IPadMini} />
+		</div>
 	);
 }
 
