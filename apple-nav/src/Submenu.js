e09824a07fe910fb1dc8a998965998343c2e441c
Jared Cuffe <jcuffe@gmail.com>
2018-04-10 14:49:08 -0700

    Angrily implemented animations

--- apple-nav/src/Submenu.js
+++ apple-nav/src/Submenu.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Transition } from 'react-transition-group'
+
+const defaultStyle = {
+  transition: 'transform 300ms ease-in-out'
+}
+
+const transitionStyles = {
+  entering: { transform: 'translate(300px)' },
+  entered: { transform: 'translate(0px)' }
+}
 
 const Submenu = ({ match, items }) => {
   return (
     <div className='Submenu'>
-      {items.map((item) => {
-        return <NavLink to={`${match.url}/${item.path}`}>{item.title}</NavLink>
-      })}
+      <Transition appear={true} in={true} timeout={0}>
+        {(state) => {
+          console.log(state);
+          return (<div
+            className='Submenu'  
+            key='submenu'  
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}>
+            {items.map((item) => {
+              return <NavLink to={`${match.url}/${item.path}`}>{item.title}</NavLink>
+            })}
+          </div>)
+        }}
+      </Transition>
     </div>
   )
 }
