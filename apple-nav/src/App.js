00e3b9217b9f4d663b03c233c4debe7233059d92
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-04-10 14:33:23 -0700

    Apple-Nav

---
+++ apple-nav/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavWrapper from './components/NavWrapper'
+import { Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+
+      <NavWrapper />
+
+    );
+  }
+}
+
+export default App;
---
+++ apple-nav/src/components/SubNav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class SubNav extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>SubNav Stuff here</div>
+        )
+    }
+}
