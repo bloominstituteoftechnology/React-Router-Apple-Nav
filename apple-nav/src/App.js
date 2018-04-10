c5498d644ae79f36cd33d250f221acfd60f3c142
unknown <bsa6.23.94@gmail.com>
2018-04-10 16:58:27 -0400

    navwrap done

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
+import Wrapper from './NavWrap.js'
 
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
+      <Wrapper  />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
