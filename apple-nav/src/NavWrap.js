7ee38570a4b2f0f87c3c19fcd7e76c117b9de265
unknown <bsa6.23.94@gmail.com>
2018-04-10 15:50:54 -0400

    first commit baseline

---
+++ apple-nav/src/NavWrap.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+const Wrapper = () => {
+    <div>
+
+        <img className="icon apple" />
+        <a className="element"></a>
+        <a className="element"></a>
+        <a className="element"></a>
+        <a className="element"></a>
+        <a className="element"></a>
+        <a className="element"></a>
+        <a className="element"></a>   
+        <img className="icon glass" />
+        <img className="icon clip" />
+        
+    </div>   
+}
