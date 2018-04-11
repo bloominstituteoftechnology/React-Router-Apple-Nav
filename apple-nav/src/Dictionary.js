4ba4b8b57bf8f51b2981e4b193a29d59476b1590
axhon <axhonruiz@gmail.com>
2018-04-10 15:34:38 -1000

    added the rest of the submenu information

--- apple-nav/src/Dictionary.js
+++ apple-nav/src/Dictionary.js
@@ -25,6 +25,55 @@ Dictionary['Mac'] = [
   'Compare'
 ]
 
+Dictionary['iPad'] = [
+  'iPad Pro',
+  'iPad',
+  'iPad mini 4',
+  'iOS 11',
+  'Accessories',
+  'Compare'
+]
+
+Dictionary['iPhone'] = [
+  'iPhone X',
+  'iPhone 8',
+  'iPhone 7',
+  'iPhone 6s',
+  'iPhone SE',
+  'iOS 11',
+  'Accessories',
+  'Compare'
+]
+
+Dictionary['Watch'] = [
+  'Apple Watch Series 3',
+  'Apple Watch Nike+',
+  'Apple Watch Hermès',
+  'Apple Watch Edition',
+  'Apple Watch Series 1',
+  'watchOS',
+  'Bands',
+  'Accessories',
+  'Compare'
+]
+
+Dictionary['TV'] = [
+  'Apple TV 4k',
+  'Apple TV',
+  'TV App',
+  'Accessories',
+  'Compare'
+]
+
+Dictionary['Music'] = [
+  'Apple Music',
+  'iTunes',
+  'HomePod',
+  'iPod touch',
+  'Music Accessories',
+  'Gift Cards'
+]
+
 export default Dictionary
 
 export { SubMenus }
