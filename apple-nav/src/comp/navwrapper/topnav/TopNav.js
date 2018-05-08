f76d2ca2f3973a7bde7614168fa78da2d895ad4e
Brandon Benefield <bsquared18@gmail.com>
2018-05-08 16:43:43 -0400

    refactor(TopNav): `TopNav` now maps over an array of objects containing information specific for each link in the navigation bar

--- apple-nav/src/comp/navwrapper/topnav/TopNav.js
+++ apple-nav/src/comp/navwrapper/topnav/TopNav.js
@@ -1,18 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TopNav = props => {
+  const navLinks = [
+    {
+      content: 'Mac',
+      url    : '/mac'
+    },
+    {
+      content: 'iPad',
+      url    : '/ipad'
+    },
+    {
+      content: 'iPhone',
+      url    : 'iphone'
+    },
+    {
+      content: 'Watch',
+      url    : '/watch'
+    },
+    {
+      content: 'TV',
+      url    : '/tv'
+    },
+    {
+      content: 'Music',
+      url    : '/music'
+    },
+    {
+      content: 'Support',
+      url    : '/support'
+    },
+  ];
+  
   return (
     <React.Fragment>
       <button>[APPLE]</button>
       
       <ul>
-        <li>Mac</li>
-        <li>iPad</li>
-        <li>iPhone</li>
-        <li>Watch</li>
-        <li>TV</li>
-        <li>Music</li>
-        <li>Support</li>
+        
+        {
+          navLinks.map(link => {
+            return (
+              <Link key={ link.content } to={ link.url }>
+                <li>{ link.content }</li>
+              </Link>
+            )
+          })
+        }
+        
       </ul>
 
       <button>[SEARCH]</button>
