376acb20a5b144c6bae7dfa37d3872530aa468fc
udykas <udykas@gmail.com>
2018-05-08 15:53:10 -0400

    Set up the App.js and index.js files

---
+++ apple-nav/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {Route} from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
