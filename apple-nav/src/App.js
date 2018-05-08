7cf9c6dd2a5e809e9e538d5c86b49db81fe07c52
Brandon Benefield <bsquared18@gmail.com>
2018-05-08 15:40:58 -0400

    feat: new CRA `apple-nav` install `react-router-dom` as new dependency

---
+++ apple-nav/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// components
+import NavWrapper from './comp/navwrapper/NavWrapper';
+
+class App extends Component {
+  render() {
+    return (
+      <NavWrapper />
+    );
+  }
+}
+
+export default App;
---
+++ apple-nav/src/comp/navwrapper/NavWrapper.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavWrapper = props => {
+  return (
+    <nav>NAVIGATION</nav>
+  );
+}
+
+export default NavWrapper;
