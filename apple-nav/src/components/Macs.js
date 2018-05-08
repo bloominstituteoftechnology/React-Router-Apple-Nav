32b2f501b094ec38bfc5f4e9d9997fb56603e6d0
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-05-08 14:36:34 -0700

    app working as it should be, not sure how I did it though lol

--- apple-nav/src/components/Macs.js
+++ apple-nav/src/components/Macs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Macs = (props) => {
+console.log(props.macs);
   return (
     <div>
         {props.macs.map(mac => <Link to={`/macs/${mac}`} key={mac}>{mac}</Link>)}
--- apple-nav/src/components/Navbar.js
+++ apple-nav/src/components/Navbar.js
@@ -8,10 +8,10 @@ class Navbar extends Component {
         this.state={
             macs: ['Macbook', 'Macbook Air', 'Macbook Pro', 'iMac', 'iMac Pro', 'Mac Pro', 'Mac Mini', 'Accessories', 'High Sierra', 'Compare'],
             ipads: ['iPad Pro', 'iPad', 'iPad mini 4', 'iOS 11', 'Accessories', 'Compare'],
-            iphone: ['iPhone X', 'iPhone 8', 'iPhone 7', 'iPhone 6s', 'iPhone SE', 'iOS 11', 'Acessories', 'Compare'],
-            watch: ['Apple Watch Series 3', 'Apple Watch Nike+', 'Apple Watch Edition', 'Apple Watch Series 1', 'WatchOS', 'Bands', 'Accessories', 'Compare'],
-            tv: ['Apple TV 4K', 'Apple TV', 'TV App', 'Accessories', 'Compare'],
-            music: ['Apple Music', 'iTunes', 'HomePod', 'iPod touch', 'Music Accessories', 'Gift Cards']
+            iphones: ['iPhone X', 'iPhone 8', 'iPhone 7', 'iPhone 6s', 'iPhone SE', 'iOS 11', 'Acessories', 'Compare'],
+            watchs: ['Apple Watch Series 3', 'Apple Watch Nike+', 'Apple Watch Edition', 'Apple Watch Series 1', 'WatchOS', 'Bands', 'Accessories', 'Compare'],
+            tvs: ['Apple TV 4K', 'Apple TV', 'TV App', 'Accessories', 'Compare'],
+            musics: ['Apple Music', 'iTunes', 'HomePod', 'iPod touch', 'Music Accessories', 'Gift Cards']
           }
     }
     render() {
@@ -19,8 +19,18 @@ class Navbar extends Component {
             <div>
                 <Link to="/">Home</Link>
                 <Link to="/macs">Macs</Link>
+                <Link to="/ipads">iPads</Link>
+                <Link to="/iphones">iPhones</Link>
+                <Link to="/watchs">Watch</Link>
+                <Link to="/tvs">TV</Link>
+                <Link to="/musics">Music</Link>
                 <Route exact path="/" component={Home} />
                 <Route path="/macs" render={(props) => <Macs {...props} macs={this.state.macs} />} />
+                <Route path="/ipads" render={(props) => <Macs {...props} macs={this.state.ipads} />} />
+                <Route path="/iphones" render={(props) => <Macs {...props} macs={this.state.iphones} />} />
+                <Route path="/watchs" render={(props) => <Macs {...props} macs={this.state.watchs} />} />
+                <Route path="/tvs" render={(props) => <Macs {...props} macs={this.state.tvs} />} />
+                <Route path="/musics" render={(props) => <Macs {...props} macs={this.state.musics} />} />
             </div>
         )
     }
