2dfcb2d278fe88db1d39ee9fb3d52ebc61fd4cbd
jesuarva <jean.ariza@jesuarva.com>
2018-05-08 22:41:46 +0200

    ADD starting Main nav-bar

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  MainNav,
+  SubNav,
+  SubNavItem
+} from './components';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MainNav />
+        
       </div>
     );
   }
 }
 
 export default App;
+ 
---
+++ apple-nav/src/components/MainNav/MainNav.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+    Navbar,
+    NavbarToggler,
+    Collapse,
+    NavbarBrand,
+    Nav,
+NavItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { CustomLink, CustomModal } from '../../components';
+
+
+
+export default class MainNav extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+      sections: ['Mac', 'iPad', 'iPhone', 'Watch', 'TV', 'Music', 'Support']
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarToggler onClick={this.toggle} />
+          <NavbarBrand href="/">Mac-icon</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+                {/* (section, i) => (<Link to={`/${section}/`}> {section} </Link>) */}
+                {this.state.sections.map(
+                    (section, i) => (<CustomLink key={i} text={section} />)
+                )}
+
+                <CustomModal buttonLabel="searchIcon" />
+                <CustomLink text="bagIcon" />
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
