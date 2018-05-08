eedc99cddd01daf60c79836a1dd1146c801e32c2
Brandon Benefield <bsquared18@gmail.com>
2018-05-08 16:46:31 -0400

    refactor(NavWrapper): remove `TopNavSub` comp

--- apple-nav/src/comp/navwrapper/NavWrapper.js
+++ apple-nav/src/comp/navwrapper/NavWrapper.js
@@ -2,14 +2,10 @@ import React from 'react';
 
 // components
 import TopNav from './topnav/TopNav';
-import TopNavSub from './topnav/TopNavSub';
 
 const NavWrapper = props => {
   return (
-    <nav>
-      <TopNav />
-      <TopNavSub />
-    </nav>
+    <TopNav />
   );
 }
 
