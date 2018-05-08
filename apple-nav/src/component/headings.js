8ae425a0adcf5205440dcb90a2443611c76a4c7f
mister-corn <code@ronaldlibago.com>
2018-05-08 11:44:53 -0700

    Adding icons for SubNavs. MVP reached.

---
+++ apple-nav/src/component/headings.js
@@ -0,0 +1,137 @@
+const headings = [
+  {
+    id: 0,
+    name: "Mac",
+    subHeadings: [
+      {
+        id: 0,
+        name: "MacBook",
+        icon: "./icons/mac/macbook_dark_large.svg"
+      },
+      {
+        id: 1,
+        name: "MacBook Air",
+        icon: "./icons/mac/macbookair_dark_large.svg"
+      },
+      {
+        id: 2,
+        name: "iMac",
+        icon: "./icons/mac/imac_dark_large.svg"
+      },
+      {
+        id: 3,
+        name: "iMac Pro",
+        icon: "./icons/mac/imac_dark_large.svg"
+      },
+      {
+        id: 4,
+        name: "Mac Pro",
+        icon: "./icons/mac/macpro_dark_large.svg"
+      },
+      {
+        id: 5,
+        name: "Mac Mini",
+        icon: "./icons/mac/macmini_dark_large.svg"
+      },
+      {
+        id: 6,
+        name: "Accessories",
+        icon: "./icons/mac/mac_acc_dark_large.svg"
+      },
+      {
+        id: 7,
+        name: "High Sierra",
+        icon: "./icons/mac/mac_osx_dark_large.svg"
+      },
+      {
+        id: 8,
+        name: "Compare",
+        icon: "./icons/mac/mac_comp_dark_large.svg"
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "iPad",
+    subHeadings: [
+      {
+        id: 0,
+        name: "iPad Pro",
+        icon: "./icons/ipad/ipadpro_light_large.svg"
+      },
+      {
+        id: 1,
+        name: "iPad",
+        icon: "./icons/ipad/ipad_light_large.svg"
+      },
+      {
+        id: 2,
+        name: "iPad mini 4",
+        icon: "./icons/ipad/ipadmini_light_large.svg"
+      },
+      {
+        id: 3,
+        name: "iOS 11",
+        icon: "./icons/ipad/ipad_ios11_light_large.svg"
+      },
+      {
+        id: 4,
+        name: "Accessories",
+        icon: "./icons/ipad/ipad_acc_light_large.svg"
+      },
+      {
+        id: 5,
+        name: "Compare",
+        icon: "./icons/ipad/ipad_comp_light_large.svg"
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "iPhone",
+    subHeadings: [
+      {
+        id: 0,
+        name: "iPhone X",
+        icon: "./icons/iphone/iphonex_large.svg"
+      },
+      {
+        id: 1,
+        name: "iPhone 8",
+        icon: "./icons/iphone/iphone_large.svg"
+      },
+      {
+        id: 2,
+        name: "iPhone 7",
+        icon: "./icons/iphone/iphone_large.svg"
+      },
+      {
+        id: 3,
+        name: "iPhone 6s",
+        icon: "./icons/iphone/iphone_large.svg"
+      },
+      {
+        id: 4,
+        name: "iPhone SE",
+        icon: "./icons/iphone/iphonese_large.svg"
+      },
+      {
+        id: 5,
+        name: "iOS 11",
+        icon: "./icons/iphone/iphone_ios11_large.svg"
+      },
+      {
+        id: 6,
+        name: "Accessories",
+        icon: "./icons/iphone/iphone_acc_large.svg"
+      },
+      {
+        id: 7,
+        name: "Compare",
+        icon: "./icons/iphone/iphone_comp_large.svg"
+      },
+    ],
+  },
+];
+
+export default headings;
---
+++ apple-nav/src/component/index.js
@@ -0,0 +1,9 @@
+import Nav from './Nav/Nav';
+import NavWrapper from './NavWrapper/NavWrapper';
+
+export {
+  Nav,
+  NavWrapper
+};
+
+// import { Nav, NavWrapper } from './component';
