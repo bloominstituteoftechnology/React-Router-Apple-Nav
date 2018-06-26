e8aa3471adf427c08ab53210de0af3329f1c3d3e
DirupT <iamcooled@gmail.com>
2018-06-26 17:33:23 -0500

    removed styled component / added alt tag (causing warnings)

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 
-
-
 class App extends Component {
   render() {
     return (
--- apple-nav/src/components/Navigation/SubNavigationItems.js
+++ apple-nav/src/components/Navigation/SubNavigationItems.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { SubNavIcon, StyledIconWrapper } from '../ReusableComponents/SubNavigation';
+import { StyledIconWrapper } from '../ReusableComponents/SubNavigation';
 import './Navigation.css';
 
 const SubNavigationItems = props => {
     return (
         <React.Fragment>
-            
+
             {props.icons.icons.map(icon => {
                 return <StyledIconWrapper className={icon.class} key={icon.text}>
-                    <img src={icon.icon}></img>
+                    <img src={icon.icon} alt=''></img>
                     <span>{icon.text}</span>
                 </StyledIconWrapper>
             })}
