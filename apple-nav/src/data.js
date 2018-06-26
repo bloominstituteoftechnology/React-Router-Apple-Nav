094ac23987a2ed2fc4f72e8f67676b4d52de1804
chainchompa <chainchompa@gmail.com>
2018-06-26 18:32:42 -0400

    initial commit

---
+++ apple-nav/src/data.js
@@ -0,0 +1,188 @@
+const Data = [
+    {
+        id: 0,
+        category: 'Apple Logo',
+    },
+    {
+        id: 1,
+        category: 'Mac',
+        subCategory: [
+            {
+                info: 'MacBook',
+            },
+            {
+                info: 'MacBook Air',
+            },
+            {
+                info: 'MacBook Pro',
+            },
+            {
+                info: 'iMac',
+            },
+            {
+                info: 'iMac Pro',
+            },
+            {
+                info: 'Mac Pro',
+            },
+            {
+                info: 'Mac mini',
+            },
+            {
+                info: 'Accessories',
+            },
+            {
+                info: 'High Sierra',
+            },
+            {
+                info: 'Compare',
+            },
+        ],
+    },
+    {
+        id: 2,
+        category: 'iPad',
+        subCategory: [
+            {
+                info: 'iPad Pro',
+            },
+            {
+                info: 'iPad',
+            },
+            {
+                info: 'iPad mini 4',
+            },
+            {
+                info: 'iOS 11',
+            },
+            {
+                info: 'Accessories',
+            },
+            {
+                info: 'Compare',
+            },
+        ],
+    },
+    {
+        id: 3,
+        category: 'iPhone',
+        subCategory: [
+            {
+                info: 'iPhone X',
+            },
+            {
+                info: 'iPhone 8',
+            },
+            {
+                info: 'iPhone 7',
+            },
+            {
+                info: 'iPhone 6s',
+            },
+            {
+                info: 'iPhone SE',
+            },
+            {
+                info: 'iOS 11',
+            },
+            {
+                info: 'Accessories',
+            },
+            {
+                info: 'Compare',
+            },
+        ],
+    },
+    {
+        id: 4,
+        category: 'Watch',
+        subCategory: [
+            {
+                info: 'Apple Watch Series 3',
+            },
+            {
+                info: 'Apple Watch Nike+',
+            },
+            {
+                info: 'Apple Watch Hermes',
+            },
+            {
+                info: 'Apple Watch Edition',
+            },
+            {
+                info: 'Apple Watch Series 1',
+            },
+            {
+                info: 'watchOS',
+            },
+            {
+                info: 'Bands',
+            },
+            {
+                info: 'Accessories',
+            },
+            {
+                info: 'Compare',
+            },
+        ],
+    },
+    {
+        id: 5,
+        category: 'TV',
+        subCategory: [
+            {
+                info: 'Apple TV 4K',
+            },
+            {
+                info: 'Apple TV',
+            },
+            {
+                info: 'TV App',
+            },
+            {
+                info: 'Accessories',
+            },
+            {
+                info: 'Compare',
+            },
+        ],
+    },
+    {
+        id: 6,
+        category: 'Music',
+        subCategory: [
+            {
+                info: 'Apple Music',
+            },
+            {
+                info: 'iTunes',
+            },
+            {
+                info: 'HomePod',
+            },
+            {
+                info: 'iPod touch',
+            },
+            {
+                info: 'Music Accessories',
+            },
+            {
+                info: 'Gift Cards',
+            },
+        ],
+    },
+    {
+        id: 7,
+        category: 'Support',
+    },
+    {
+        id: 8,
+        category: 'Search Icon',
+    },
+    {
+        id: 9,
+        category: 'Shopping Icon',
+    },
+]
+
+export default Data;
