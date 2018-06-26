7a0a4f731a01978ab5647b617138d25bb62f2ab1
chainchompa <chainchompa@gmail.com>
2018-06-26 18:36:08 -0400

    added navwrapper

---
+++ apple-nav/src/Components/NavWrapper.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Data from '../Data';
+
+class NavWrapper extends React.Component {
+  constructor(props) {
+    super(props)''
+    this.state = {
+      data:Data
+    }
+  }
+  render() {
+    return {
+      <div>
+      </div>
+    }
+  }
+}
+
+export default NavWrapper;
