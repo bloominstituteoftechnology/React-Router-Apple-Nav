cdbf01375b8db0b472500186fd97612c3d04406b
Gabriel Duquette <gabrielduquette@gmail.com>
2018-06-28 01:14:09 -0400

    add icons, styling

--- apple-nav/src/components/SubNav.js
+++ apple-nav/src/components/SubNav.js
@@ -2,25 +2,20 @@ import React from 'react';
 import '../App.css';
 import navLinks from '../data';
 
-class SubNav extends React.Component {
-  state = {
-    subNav: []
-  }
-
-  componentWillMount() {
-    const subNav = navLinks.find(x => x.name === this.props.match.params.name);
-    this.setState({ subNav: subNav.subNav });
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.subNav.map(item => (
-          <div key={Math.random()}>{item}</div>
+const SubNav = props => {
+  const topLink = navLinks.find(x => x.name === props.match.params.name);
+  return (
+    <div class="subnav-container-container">
+      <div class="subnav-container">
+        {topLink.subNav.map(item => (
+          <div className="icon-name-container" key={Math.random()}>
+            <img src={item.icon} alt="An Apple product."/>
+            <div>{item.name}</div>
+          </div>
         ))}
-      </div>
-    );
-  }
+    </div>
+    </div>
+  );
 }
 
 export default SubNav;
