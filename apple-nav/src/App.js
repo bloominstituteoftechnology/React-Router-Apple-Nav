cc8b1f21d692c5cc38c19d4f34755debab3bcf22
sliw <slioweng@gmail.com>
2018-08-07 05:24:37 +0800

    mock up the apple nav bar

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -6,25 +6,45 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavLink to="/1" activeClassName="isSelected">1</NavLink>
-        <NavLink to="/2" activeClassName="isSelected">2</NavLink>
-        <NavLink to="/3" activeClassName="isSelected">3</NavLink>
-        <NavLink to="/4" activeClassName="isSelected">4</NavLink>
-        <NavLink to="/5" activeClassName="isSelected">5</NavLink>
+        <NavLink to="/1" activeClassName="isSelected">1 item</NavLink>
+        <NavLink to="/2" activeClassName="isSelected">2 item</NavLink>
+        <NavLink to="/3" activeClassName="isSelected">3 item</NavLink>
+        <NavLink to="/4" activeClassName="isSelected">4 item</NavLink>
+        <NavLink to="/5" activeClassName="isSelected">5 item</NavLink>
 
-        <Route exact path="/" component={() => testingHeader(1)}/>
-        <Route exact path="/1" component={() => testingHeader(1)}/>
-        <Route exact path="/2" component={() => testingHeader(2)}/>
-        <Route exact path="/3" component={() => testingHeader(3)}/>
-        <Route exact path="/4" component={() => testingHeader(4)}/>
-        <Route exact path="/5" component={() => testingHeader(5)}/>
+        <Route exact path="/" component={() => TestingHeader(1)}/>
+        <Route exact path="/1" component={() => TestingHeader(1)}/>
+        <Route exact path="/2" component={() => TestingHeader(2)}/>
+        <Route exact path="/3" component={() => TestingHeader(3)}/>
+        <Route exact path="/4" component={() => TestingHeader(4)}/>
+        <Route exact path="/5" component={() => TestingHeader(5)}/>
       </div>
     );
   }
 }
 
-function testingHeader(num) {
-  return <h1>Testing{`${num}`}</h1>
+function TestingHeader(num) {
+  return (
+    <div>
+      <TestingList num={num} />
+      <h1>Main page - {`${num}`}</h1>
+    </div>
+  )
+}
+
+function TestingList(props) {
+  const { num } = props;
+  return (
+    <div>
+      <li className="catergory-nav">
+        <ul>1. for nav {num} </ul>
+        <ul>2. for nav {num} </ul>
+        <ul>3. for nav {num} </ul>
+        <ul>4. for nav {num} </ul>
+        <ul>5. for nav {num} </ul>
+      </li>
+    </div>
+  )
 }
 
 export default App;
