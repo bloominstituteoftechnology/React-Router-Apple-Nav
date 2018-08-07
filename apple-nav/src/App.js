c529312824ab5abd5c37f6bd140b490e87d6ffc5
Sean <sean47900@gmail.com>
2018-08-07 15:55:34 -0500

    update

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import SubNav from './Components/SubNav';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+              <div className="app-container">
+          <Route exact path="/:product" component={SubNav} />
+        </div>
       </div>
     );
   }
