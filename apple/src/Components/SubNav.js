979e4f582703e0910c643ca32143dd5b48f686b1
Trevor <trevorfehrman@gmail.com>
2018-08-07 16:16:33 -0700

    Styling progress

--- apple/src/Components/SubNav.js
+++ apple/src/Components/SubNav.js
@@ -1,25 +1,41 @@
 import React from "react";
 import styled from "styled-components";
 
+const SubNavWrapperWrapper = styled.div`
+	padding: 10px 30px;
+	background-color: ${props => props.color};
+`;
+
 const SubNavWrapper = styled.div`
 	display: flex;
 	justify-content: space-between;
-background-color: ${ props => props.color };
-	color: white;
-	font-family: 'Roboto', sans-serif;
+	align-items: center;
+	color: ${props => props.font};
+	font-family: "Roboto", sans-serif;
 	font-size: 1.3rem;
-	padding: 10px 30px;
+	margin: 0 auto;
+	max-width: 800px;
 `;
 
 const SubNav = props => {
 	let linkName = props.match.params.name;
+	let dark = linkName === "Mac" || linkName === "TV";
+	let color = dark ? "rgba(0, 0, 0, 0.83)" : "rgba(242, 242, 242, 0.7)";
+	let font = dark ? "white" : "rgb(17, 17, 17)";
 	return (
-		<SubNavWrapper>
-			{props.data.length > 0 &&
-				props.data
-					.find(e => e.name === props.match.params.name)
-					.subLinks.map(ele => <div><img src={ele.img} alt=""/><p>{ele.name}</p></div>)}
-		</SubNavWrapper>
+		<SubNavWrapperWrapper color={color}>
+			<SubNavWrapper font={font}>
+				{props.data.length > 0 &&
+					props.data
+						.find(e => e.name === props.match.params.name)
+						.subLinks.map(ele => (
+							<div>
+								<img src={ele.img} alt="" />
+								<p>{ele.name}</p>
+							</div>
+						))}
+			</SubNavWrapper>
+		</SubNavWrapperWrapper>
 	);
 };
 
--- apple/src/Components/TopNav.js
+++ apple/src/Components/TopNav.js
@@ -2,40 +2,62 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-const TopNavWrapper = styled.div`
+const TopNavWrapperWrapper = styled.div`
 	display: flex;
-	justify-content: center;
 	align-items: center;
 	background: black;
-	padding: 10px 30px;
 	opacity: 0.83;
-	height: 30px;
+	padding: 0 30px;
+	height: 40px;
+`;
+
+const TopNavWrapper = styled.div`
+	margin: 0 auto;
+	height: 100%;
+	max-width: 1024px;
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 const TopNav__link = styled(NavLink)`
 	color: white;
 	text-decoration: none;
-	font-family: 'Roboto', sans-serif;
+	font-family: "Roboto", sans-serif;
 	font-size: 1.1rem;
-	padding: 0 3%;
-	&.active{
+	&:not(:last-child) {
+		margin-right: 2rem;
+	}
+	&.active {
 		opacity: 0.7;
 	}
 `;
 
 function TopNav(props) {
-	let logo = "https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/apple/image_small.svg"
-	let spyglass = "https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/search/image_large.svg"
-	let cart = "https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/bag/image_large.svg"
+	let logo =
+		"https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/apple/image_small.svg";
+	let spyglass =
+		"https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/search/image_large.svg";
+	let cart =
+		"https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/bag/image_large.svg";
 	return (
-		<TopNavWrapper>
-			<TopNav__link to='/'><img src={logo} alt=""/></TopNav__link>
-			{props.data.map( e => (
-				<TopNav__link to={`/${e.name}`}>{e.name}</TopNav__link>
-			))}
-			<TopNav__link to='/'><img src={spyglass} alt=""/></TopNav__link>
-			<TopNav__link to='/'><img src={cart} alt=""/></TopNav__link>
-		</TopNavWrapper>
+		<TopNavWrapperWrapper>
+			<TopNavWrapper>
+				<TopNav__link to="/">
+					<img src={logo} alt="" />
+				</TopNav__link>
+				{props.data.map(e => (
+					<TopNav__link to={`/${e.name}`}>{e.name}</TopNav__link>
+				))}
+				<TopNav__link to="/">
+					<img src={spyglass} alt="" />
+				</TopNav__link>
+				<TopNav__link to="/">
+					<img src={cart} alt="" />
+				</TopNav__link>
+			</TopNavWrapper>
+		</TopNavWrapperWrapper>
 	);
 }
 
