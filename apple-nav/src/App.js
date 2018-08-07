09a79c72a016b03ba6bb1fd8bab3c67d09286f38
keegn <keegan.t.burkett@gmail.com>
2018-08-07 15:35:26 -0500

    added react router and wrapped App component in the router

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
