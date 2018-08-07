d23806171126af677baf430a72ddec4f89cc425e
codejoncode <jonathanjholloway@gmail.com>
2018-08-07 15:21:48 -0500

    set up the project and I have the home page set up with all the tags and icons neccessary to mimick the apple.com page

---
+++ apple-nav/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomeNav from './components/HomeNav';
+
+class App extends Component {
+  render() {
+    return (
+      
+      <Fragment>
+          <HomeNav />
+      </Fragment>
+    );
+  }
+}
+// Apple Logo  Mac iPad iPhone Watch TV Music Support *search thingy*  notepade 
+export default App;
---
+++ apple-nav/src/components/HomeNav.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Route, Link} from 'react-router-dom';
+import styled from 'styled-components'; 
+
+const navTags = ["Mac", "iPad", "iPhone", "Watch", "TV", "Music", "Support"];
+
+const NavHeader = styled.div`
+    display: flex; 
+    position: fixed;
+    color: white;
+    background: black;
+    width: 100%; 
+    z-index: 1;
+    height: 50px;
+    justify-content: center;
+`;
+
+const NavBox = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 900px;
+    margin-left: 250px;
+    margin-right: 250px;
+`;
+
+class HomeNav extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+
+    render(){
+
+        return (
+            <NavHeader>
+                <NavBox>
+                    <i class="fab fa-apple"></i>
+                    {navTags.map( tag => <div>{tag}</div>)}
+                    <i class="fas fa-search"></i>
+                    <i class="far fa-clipboard"></i>
+                </NavBox>
+                
+            </NavHeader>
+
+        );
+    }
+}
+
+export default HomeNav; 
