abae2c08a7dc1204069c319edf17cfe97f93a63a
Carey <careycbaldwin@gmail.com>
2018-08-07 16:11:01 -0400

    installed react-router-dom and wrapped App.js in Router component

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,10 +13,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <ul className = "navbar">
+          <li>
+            <NavLink exact activeClassName="activeNavButton" to = "/">
+              Home
+            </NavLink>
+          </li>  
+        </ul>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
