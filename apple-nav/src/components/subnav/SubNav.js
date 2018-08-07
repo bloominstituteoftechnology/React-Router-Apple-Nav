e5b192861e55b21c822127532151b40c5d61925c
szincone <sawyerzincone@gmail.com>
2018-08-07 16:09:04 -0600

    navbar linking to where I want it to

--- apple-nav/src/components/subnav/SubNav.js
+++ apple-nav/src/components/subnav/SubNav.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./SubNav.css";
+import ProductPage from "../productPage/ProductPage";
 
 export default class SubNav extends Component {
   constructor(props) {
@@ -15,7 +16,13 @@ export default class SubNav extends Component {
       <div className="subNavWrapper">
         <ul>
           {this.state.data.map(product => (
-            <Link className="productCardSubLink" to={product.url}>
+            <Link
+              className="productCardSubLink"
+              to={`/productPage/${product.productName
+                .toLowerCase()
+                .split(" ")
+                .join("-")}`}
+            >
               <img src={product.imgUrl} />
               <li>{product.productName}</li>
             </Link>
