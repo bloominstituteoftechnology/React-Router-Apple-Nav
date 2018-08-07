5dd66d96e675e141cd7e804a43dc5f08fd504f99
Trevor <trevorfehrman@gmail.com>
2018-08-07 15:35:34 -0700

    Styling practice

--- apple/src/Components/SubNav.js
+++ apple/src/Components/SubNav.js
@@ -3,21 +3,22 @@ import styled from "styled-components";
 
 const SubNavWrapper = styled.div`
 	display: flex;
-`;
-
-const SubNav__link = styled.div`
-	background-color: black;
+	justify-content: space-between;
+background-color: ${ props => props.color };
 	color: white;
+	font-family: 'Roboto', sans-serif;
+	font-size: 1.3rem;
+	padding: 10px 30px;
 `;
 
 const SubNav = props => {
-	console.log(props.match.params.name);
+	let linkName = props.match.params.name;
 	return (
 		<SubNavWrapper>
 			{props.data.length > 0 &&
 				props.data
 					.find(e => e.name === props.match.params.name)
-					.subLinks.map(ele => <div>{ele.name}</div>)}
+					.subLinks.map(ele => <div><img src={ele.img} alt=""/><p>{ele.name}</p></div>)}
 		</SubNavWrapper>
 	);
 };
--- apple/src/Components/TopNav.js
+++ apple/src/Components/TopNav.js
@@ -1,22 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const TopNavWrapper = styled.div`
 	display: flex;
+	justify-content: center;
+	align-items: center;
+	background: black;
+	padding: 10px 30px;
+	opacity: 0.83;
+	height: 30px;
 `;
 
-const TopNav__link = styled(Link)`
-	background-color: black;
+const TopNav__link = styled(NavLink)`
 	color: white;
+	text-decoration: none;
+	font-family: 'Roboto', sans-serif;
+	font-size: 1.1rem;
+	padding: 0 3%;
+	&.active{
+		opacity: 0.7;
+	}
 `;
 
 function TopNav(props) {
+	let logo = "https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/apple/image_small.svg"
+	let spyglass = "https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/search/image_large.svg"
+	let cart = "https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/bag/image_large.svg"
 	return (
 		<TopNavWrapper>
+			<TopNav__link to='/'><img src={logo} alt=""/></TopNav__link>
 			{props.data.map( e => (
-			<TopNav__link to={`/${e.name}`}>{e.name}</TopNav__link>
+				<TopNav__link to={`/${e.name}`}>{e.name}</TopNav__link>
 			))}
+			<TopNav__link to='/'><img src={spyglass} alt=""/></TopNav__link>
+			<TopNav__link to='/'><img src={cart} alt=""/></TopNav__link>
 		</TopNavWrapper>
 	);
 }
