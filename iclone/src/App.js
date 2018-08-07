550d329e33c2d724069b4194c5b6300889d3294f
rodacoram <rodacoram8@gmail.com>
2018-08-07 14:57:01 -0600

    menu-clicks lead to submenu url

--- iclone/src/App.js
+++ iclone/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
+import SubMenu from './components/SubMenu';
 import './App.css';
+
+const menu = {
+  menuItems: ['Mac','iPad','iPhone','Watch','TV','Music']
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">iClone</h1>
+          <div className="nav-buttons">
+                {menu.menuItems.map(item=> <Link to={`/${item}`}>
+                    {item}
+                </Link>
+                )}
+            </div>
+          <Route
+            path="/:subMenu"
+            render={props => <SubMenu {...props} />}
+          />
         </header>
         <p className="App-intro">
           Consume, Buy, Obey
---
+++ iclone/src/components/SubMenu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function SubMenu(props) {
+    return(
+        <div>
+        </div>
+    )
+}
+
+export default SubMenu;
