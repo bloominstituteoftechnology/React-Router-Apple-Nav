86ff3e29a4c39ff128b471a6ec60f4dfc3f0539b
szincone <sawyerzincone@gmail.com>
2018-08-07 15:19:44 -0600

    creted mac subnav data array with img links and names

--- apple-nav/src/components/Mac/Mac.js
+++ apple-nav/src/components/Mac/Mac.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import SubNav from "../subnav/SubNav";
+import macSubNavData from "./macSubNavData";
 
 export default class Mac extends Component {
   render() {
---
+++ apple-nav/src/components/mac/macSubNavData.js
@@ -0,0 +1,24 @@
+const macSubNavData = [
+  {
+    productName: "MacBook",
+    imgUrl:
+      "https://www.apple.com/v/mac/home/ab/images/familybrowser/macbook_dark_large.svg"
+  },
+  {
+    productName: "MacBook Air",
+    imgUrl:
+      "https://www.apple.com/v/mac/home/ab/images/familybrowser/macbookair_dark_large.svg"
+  },
+  {
+    productName: "MacBook Pro",
+    imgUrl:
+      "https://www.apple.com/v/mac/home/ab/images/familybrowser/macbookpro_dark_large.svg"
+  },
+  {
+    productName: "iMac",
+    imgUrl:
+      "https://www.apple.com/v/mac/home/ab/images/familybrowser/imac_pro_dark_large.svg"
+  }
+];
+
+export default macSubNavData;
