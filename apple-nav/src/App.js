a10f5733800498a5be5547390ccb56c34b7ea621
William <kwontaewoongah@gmail.com>
2018-08-07 14:48:55 -0700

    finished Navwrapper and Nav components

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {NavLink} from 'react-router-dom';
 import './App.css';
+import styled from 'styled-components';
+import Nav from './Nav.js';
 
+const NavWrapper=styled.ul`
+  max-width: 800px;
+  width: 100%;
+  height: 100px;
+  margin: 0 auto;
+  background-color: black;
+  opacity: 0.8;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  list-style-type: none;
+`;
+const NavArr=[
+  'Mac',
+  'iPad',
+  'iPhone',
+  'Watch',
+  'TV',
+  'Music', 
+  'Support'
+  
+]
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <NavWrapper>
+         <li>
+           <NavLink exact activeClassName='activeNavLink' to="/"><i className="fab fa-apple"></i></NavLink>
+         </li>
+         {NavArr.map(e=><Nav data={e}/>)}
+         <li>
+           <i class="fas fa-search"></i>
+         </li>
+         <li>
+           <i class="fas fa-shopping-bag"></i>
+         </li>
+       </NavWrapper>
       </div>
     );
   }
