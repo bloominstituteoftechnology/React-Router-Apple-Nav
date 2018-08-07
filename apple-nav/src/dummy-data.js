66c1eacd37f298faaff03ab42a150285fc894d9a
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-08-07 15:47:49 -0400

    Created a bunch of dummy-data for the nav bar

---
+++ apple-nav/src/dummy-data.js
@@ -0,0 +1,227 @@
+export default dummyData = [
+  {
+    id: 0,
+    text: 'Mac',
+    SubNavs: [
+      {
+        id: 0,
+        text: 'MacBook',
+      },
+      {
+        id: 1,
+        text: 'MacBook Air',
+      },
+      {
+        id: 2,
+        text: 'MacBook Pro',
+      },
+      {
+        id: 3,
+        text: 'iMac',
+      },
+      {
+        id: 4,
+        text: 'MacBook',
+      },
+      {
+        id: 5,
+        text: 'iMac Pro',
+      },
+      {
+        id: 6,
+        text: 'Mac Pro',
+      },
+      {
+        id: 7,
+        text: 'Mac mini',
+      },
+      {
+        id: 8,
+        text: 'Accessories',
+      },
+      {
+        id: 9,
+        text: 'High Sierra',
+      },
+    ],
+  },
+  {
+    id: 1,
+    text: 'iPad',
+    SubNavs: [
+      {
+        id: 0,
+        text: 'iPad Pro',
+      },
+      {
+        id: 1,
+        text: 'iPad',
+      },
+      {
+        id: 2,
+        text: 'iPad mini 4',
+      },
+      {
+        id: 3,
+        text: 'iOS 11',
+      },
+      {
+        id: 4,
+        text: 'Accessories',
+      },
+      {
+        id: 5,
+        text: 'Compare',
+      },
+    ],
+  },
+  {
+    id: 2,
+    text: 'iPhone',
+    SubNavs: [
+      {
+        id: 0,
+        text: 'iPhone X',
+      },
+      {
+        id: 1,
+        text: 'iPhone 8',
+      },
+      {
+        id: 2,
+        text: 'iPhone 7',
+      },
+      {
+        id: 3,
+        text: 'iPhone 6s',
+      },
+      {
+        id: 4,
+        text: 'iPhone SE',
+      },
+      {
+        id: 5,
+        text: 'iOS 11',
+      },
+      {
+        id: 6,
+        text: 'Accessories',
+      },
+      {
+        id: 7,
+        text: 'Compared',
+      },
+      {
+        id: 8,
+        text: 'Accessories',
+      },
+    ],
+  },
+  {
+    id: 3,
+    text: 'Watch',
+    SubNavs: [
+      {
+        id: 0,
+        text: 'Apple Watch Series 3',
+      },
+      {
+        id: 1,
+        text: 'Apple Watch Nike+',
+      },
+      {
+        id: 2,
+        text: 'Apple Watch Hermes',
+      },
+      {
+        id: 3,
+        text: 'Apple Watch Edition',
+      },
+      {
+        id: 4,
+        text: 'Apple Watch Series 1',
+      },
+      {
+        id: 5,
+        text: 'watchOS',
+      },
+      {
+        id: 6,
+        text: 'Bands',
+      },
+      {
+        id: 7,
+        text: 'Compared',
+      },
+    ],
+  },
+  {
+    id: 4,
+    text: 'TV',
+    SubNavs: [
+      {
+        id: 0,
+        text: 'Apple TV 4K',
+      },
+      {
+        id: 1,
+        text: 'Apple TV',
+      },
+      {
+        id: 2,
+        text: 'TV App',
+      },
+      {
+        id: 3,
+        text: 'Accessories',
+      },
+      {
+        id: 4,
+        text: 'Compare',
+      },
+    ],
+  },
+  {
+    id: 0,
+    text: 'Music',
+    SubNavs: [
+      {
+        id: 0,
+        text: 'Apple Music',
+      },
+      {
+        id: 1,
+        text: 'iTunes',
+      },
+      {
+        id: 2,
+        text: 'HomePod',
+      },
+      {
+        id: 3,
+        text: 'iPod Touch',
+      },
+      {
+        id: 4,
+        text: 'Music Accessories',
+      },
+      {
+        id: 5,
+        text: 'Gift Cards',
+      },
+      {
+        id: 6,
+        text: 'Mac Pro',
+      },
+      {
+        id: 7,
+        text: 'Mac mini',
+      },
+    ],
+  },
+  {
+    id: 0,
+    text: 'Support',
+    SubNavs: [],
+  },
+];
