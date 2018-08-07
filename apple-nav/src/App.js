f92629d26dcd901ed22112225af42ecdb87277d5
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-08-07 18:03:21 -0500

    Working on MVP
    Got my nav up trying to get the subnavs to show up

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -3,12 +3,86 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import {Route, Link, NavLink} from 'react-router-dom'; 
 import NavWrapper from './NavWrapper'; 
-
+const subNav=[
+  {
+  id:0,
+  subTabs:[
+  "none"
+   ]},
+  {
+  id:1,
+  subTabs:[
+    "MacBook",
+    "MacBook Air",
+    "MacBook Pro",
+    "iMac", 
+    "Mac Pro", 
+    "Mac Mini", 
+    "Accessories", 
+    "High Sierra", 
+    "Compare",
+   ]},
+   {
+    id:2,
+    subTabs:[
+      "iPad Pro",
+      "iPad", 
+      "iPad mini 4", 
+      "iOS 11", 
+      "Accessories", 
+      "Compare",
+     ]},
+     {
+      id:3,
+      subTabs:[
+        "iPhone X",
+        "iPhone 8", 
+        "iPhone 7", 
+        "iPhone 6s", 
+        "iPhone SE", 
+        "iOS 11", 
+        "Accessories", 
+        "Compare",
+       ]},
+       {
+        id:4,
+        subTabs:[
+          "Apple Watch Series 3",
+          "Apple Watch Nike +",
+          "Apple Watch Hermes",
+          "Apple Watch Edition", 
+          "Apple Watch Series 1", 
+          "watchOs", 
+          "Bands", 
+          "Accessories", 
+          "Compare",
+         ]},
+         {
+          id:5,
+          subTabs:[
+            "Apple TV 4k",
+            "Apple TV",
+            "TV App",
+            "Accessories",  
+            "Compare",
+           ]},
+           {
+            id:6,
+            subTabs:[
+              "Apple Music",
+              "iTunes",
+              "HomePod",
+              "iPod Touch", 
+              "Music Accessories", 
+              "Gift Cards", 
+             ]},
+]
 class App extends Component {
   render() {
     return (
    <div>
      <NavWrapper />
+     < Route/>
    </div>
     );
   }
--- apple-nav/src/Nav.js
+++ apple-nav/src/Nav.js
@@ -2,49 +2,28 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
 import { Route, Link, NavLink } from "react-router-dom";
+import SubNav from "./SubNav";
+const navItems = [
+    {name: "home",id: 0},
+    {name: "mac", id: 1 },
+    {name: "ipad", id: 2 },
+    {name: "iphone", id: 3 },
+    {name: "watch", id: 4 },
+    {name: "tv", id: 5 },
+    {name: "music", id: 6},
 
-const Nav = props => {
+]
+const Nav=(props) =>{
   return (
-    <div>
-      <div className="nav-bar">
+    <div className="nav-bar">
+    {navItems.map(item =>
         <div>
-            <NavLink className="item" to="/">
-            Home
+            <NavLink to={`/${item.name}`}>
+            {item.name}
             </NavLink>
         </div>
-        <div>
-            <NavLink className="item"  to="/mac">
-            Mac
-            </NavLink>
-        </div>
-        <div>
-            <NavLink className="item"  to="/ipad">
-            iPad
-            </NavLink>
-        </div>
-        <div>
-            <NavLink className="item"  to="/iphone">
-            iPhone
-            </NavLink>
-        </div>
-        <div>
-            <NavLink className="item"  to="/watch">
-            Watch
-            </NavLink>
-        </div>
-        <div>
-            <NavLink className="item"  to="/tv">
-            TV
-            </NavLink>
-        </div>
-        <div>
-            <NavLink className="item"  to="/music">
-            Music
-            </NavLink>
-        </div>
-      </div>
+        )}
     </div>
   ); 
 };
-
 export default Nav;
