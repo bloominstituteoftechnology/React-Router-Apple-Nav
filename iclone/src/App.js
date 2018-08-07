c979fc009a8902a7345a59a5b917a89ac6bb49c9
rodacoram <rodacoram8@gmail.com>
2018-08-07 15:46:15 -0600

    basic functionality

--- iclone/src/App.js
+++ iclone/src/App.js
@@ -4,7 +4,13 @@ import SubMenu from './components/SubMenu';
 import './App.css';
 
 const menu = {
-  menuItems: ['Mac','iPad','iPhone','Watch','TV','Music']
+  menuItems: ['Mac','iPad','iPhone','Watch','TV','Music'],
+  macItems: ['MacBook','MacBook Air','MacBook Pro', 'iMac', 'Mac Pro', 'Mac Mini', 'Accessories'],
+  ipadItems: ['iPad', 'iPad Pro', 'iPad mini 4', 'Accessories'],
+  iphoneItems: ['iPhone X', 'iPhone 8', 'iPhone 7', 'iPhone 6s', 'iPhone SE', 'Accessories'],
+  watchItems: ['Apple Watch Series 3', 'Apple Watch Nike+', 'Apple Watch Hermes', 'Apple Watch Edition', 'Apple Watch Series 1', 'Bands', 'Accessories'],
+  tvItems: ['AppleTV 4K', 'AppleTV', 'TVApp', 'Accessories'],
+  musicItems: ['Apple Music', 'iTunes', 'HomePod', 'iPod Touch', 'Music Accessories']
 }
 
 class App extends Component {
@@ -14,14 +20,21 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">iClone</h1>
           <div className="nav-buttons">
-                {menu.menuItems.map(item=> <Link to={`/${item}`}>
-                    {item}
+                {menu.menuItems.map((item,i)=> <Link key={i} to={`/${item}`}>
+                    <button>{item}</button>
                 </Link>
                 )}
             </div>
           <Route
             path="/:subMenu"
-            render={props => <SubMenu {...props} />}
+            render={props => <SubMenu macItems={menu.macItems}
+                                      ipadItems={menu.ipadItems}
+                                      iphoneItems={menu.iphoneItems}
+                                      watchItems={menu.watchItems}
+                                      tvItems={menu.tvItems}
+                                      musicItems={menu.musicItems}
+                                                {...props} 
+                                                />}
           />
         </header>
         <p className="App-intro">
--- iclone/src/components/SubMenu.js
+++ iclone/src/components/SubMenu.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function SubMenu(props) {
-    return(
+   const submenu = props.match.params.subMenu.toLowerCase()+'Items';
+   console.log(props[submenu]);
+   return(
         <div>
+            {props[submenu].map((subitem,i)=>
+            <button key={i}>
+                {subitem}
+            </button>
+            )}
         </div>
     )
 }
