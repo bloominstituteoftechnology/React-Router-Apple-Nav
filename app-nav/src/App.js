2e3bfc07e8a28f70a14769aa1fd5e6aaa7ecd9c3
Sean <sean47900@gmail.com>
2018-08-07 17:45:10 -0500

    had to restart project

---
+++ app-nav/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NavWrapper from'./Components/NavWrapper'
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavWrapper />
+      </div>
+    );
+  }
+}
+ export default App;
