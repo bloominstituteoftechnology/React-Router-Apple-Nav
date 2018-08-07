1a008d92d9a7e46803840e56375efb481fbe0b6c
Carey <careycbaldwin@gmail.com>
2018-08-07 18:41:36 -0400

    added component pages for products

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -10,16 +10,7 @@ import logo from './logo.svg';
 import NavBar from './components/Nav';
 
 import './App.css';
-
-
-const products = [
-  { name: 'mac', id: 1},
-  { name: 'ipad', id: 2},
-  { name: 'phone', id: 3},
-  { name: 'watch', id: 4},
-  { name: 'TV', id: 5},
-  { name: 'music', id: 6},
-];
+import products from './data';
 
 function Products() {
   return (
---
+++ apple-nav/src/data.js
@@ -0,0 +1,10 @@
+const products = [
+    { name: 'mac', id: 1},
+    { name: 'ipad', id: 2},
+    { name: 'phone', id: 3},
+    { name: 'watch', id: 4},
+    { name: 'TV', id: 5},
+    { name: 'music', id: 6},
+  ];
+
+export default products;
