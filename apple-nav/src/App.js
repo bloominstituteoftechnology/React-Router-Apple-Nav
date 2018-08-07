cdde4eaf3fba7abf84655b809e718df1ad86f8bc
Carey <careycbaldwin@gmail.com>
2018-08-07 17:50:22 -0400

    watching video lesson and adding Products, Product components

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -12,16 +12,44 @@ import NavBar from './components/Nav';
 import './App.css';
 
 
+const products = [
+  { name: 'mac', id: 1},
+  { name: 'ipad', id: 2},
+  { name: 'phone', id: 3},
+  { name: 'watch', id: 4},
+  { name: 'TV', id: 5},
+  { name: 'music', id: 6},
+]
+
+function Products() {
+  return (
+    <ul>
+      {products.map(product => (
+        <li key = {product.id} >
+        <h4> {product.name}</h4>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Product() {
+  return <h1>product</h1>
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
+      <div>
+        <Link to = '/products'>Products</Link>
+        <Route path = '/people' component={Products} />
+        <Route path = '/people/:id' component={Product} />
+      </div>   
+
     );
   }
 }
