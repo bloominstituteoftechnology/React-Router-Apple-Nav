68e077a512f0b98e5380ff50cea2324da842c015
Ashwin <aswinjeganath@gmail.com>
2018-08-08 05:24:27 +0530

    Add react transition group to animate on routing

--- apple-nav/src/Nav.js
+++ apple-nav/src/Nav.js
@@ -5,15 +5,38 @@ import styled from 'styled-components';
 
 import { StyledNavLink, NavContainer, ListItems } from './styles';
 
-const Nav = ({ links }) => {
+const AnimationWrapper = styled.div`
+  transition-duration: 0.3s;
+  transition-property: ${props => (props.state === 'entered' ? 'all' : 'none')};
+  transform: ${props => {
+    switch (props.state) {
+      case 'entering':
+        return 'translateX(30px)';
+      case 'entered':
+        return 'translate(0)';
+    }
+  }};
+  opacity: ${props => {
+    switch (props.state) {
+      case 'entering':
+        return 0;
+      case 'entered':
+        return 1;
+    }
+  }};
+`;
+
+const Nav = ({ links, state }) => {
   return (
-    <NavContainer>
-      {links.map(link => (
-        <ListItems key={link.text}>
-          <StyledNavLink to={`/${link.href}`}>{link.text}</StyledNavLink>
-        </ListItems>
-      ))}
-    </NavContainer>
+    <AnimationWrapper state={state}>
+      <NavContainer>
+        {links.map(link => (
+          <ListItems key={link.text}>
+            <StyledNavLink to={`/${link.href}`}>{link.text}</StyledNavLink>
+          </ListItems>
+        ))}
+      </NavContainer>
+    </AnimationWrapper>
   );
 };
 
--- apple-nav/src/RouterNav.js
+++ apple-nav/src/RouterNav.js
@@ -1,27 +1,46 @@
 import React from 'react';
-
+import { Transition } from 'react-transition-group';
 import _ from 'lodash';
 
 import Nav from './Nav';
 
-const RouterNav = ({
-  linkCollection,
-  match: {
-    url,
-    params: { name }
-  }
-}) => {
-  const tab = _.find(linkCollection, ['name', name]);
-  if (!tab) return null;
+class RouterNav extends React.Component {
+  state = {
+    in: true
+  };
 
-  return (
-    <Nav
-      links={tab.subLinks.map(link => ({
-        href: `${name}/${link.name}`,
-        text: link.name
-      }))}
-    />
-  );
-};
+  componentWillReceiveProps() {
+    this.setState({ in: false }, () => this.setState({ in: true }));
+  }
+
+  render() {
+    const {
+      linkCollection,
+      match: {
+        params: { name }
+      }
+    } = this.props;
+
+    const tab = _.find(linkCollection, ['name', name]);
+    if (!tab) return null;
+
+    return (
+      <Transition in={this.state.in} timeout={300} unmountOnExit>
+        {state => {
+          console.log(state);
+          return (
+            <Nav
+              state={state}
+              links={tab.subLinks.map(link => ({
+                href: `${name}/${link.name}`,
+                text: link.name
+              }))}
+            />
+          );
+        }}
+      </Transition>
+    );
+  }
+}
 
 export default RouterNav;
