df450451139e77fd009c42d8b91162ce2ea478b0
MichaelBoegner <cmikeb@gmail.com>
2018-08-07 17:47:07 -0400

    Charles Boegner
    Navbar styling completed. replicated mac site. adding subnavbar component to mac navlink

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -2,26 +2,32 @@ import React, { Component } from 'react';
 import {NavLink, Route} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
+import dummyData from './data';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+      navData: dummyData
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <div className="navbar">
-          <div className='navlink'>
-            <NavLink exact to="/"><i class="fab fa-apple"></i></NavLink>
-            <NavLink to="/mac">Mac</NavLink>
-            <NavLink to="/ipad">iPad</NavLink>
-            <NavLink to="/iphone">iPhone</NavLink>
-            <NavLink to="/watch">Watch</NavLink>
-            <NavLink to="/tv">TV</NavLink>
-            <NavLink to="/music">Music</NavLink>
-            <NavLink to="/support">Support</NavLink>
-            <NavLink to="/search"><i class="fas fa-search"></i></NavLink>
-            <NavLink to="/cart"><i class="fas fa-shopping-bag"></i></NavLink>
-          </div>
-          <Route path="/" Component={Home}/>
-        </div>
+            <NavLink className="navlink" exact to="/home"><i class="fab fa-apple"></i></NavLink>
+            <NavLink className="navlink" to="/mac">Mac</NavLink>
+            <NavLink className="navlink" to="/ipad">iPad</NavLink>
+            <NavLink className="navlink" to="/iphone">iPhone</NavLink>
+            <NavLink className="navlink" to="/watch">Watch</NavLink>
+            <NavLink className="navlink" to="/tv">TV</NavLink>
+            <NavLink className="navlink" to="/music">Music</NavLink>
+            <NavLink className="navlink" to="/support">Support</NavLink>
+            <NavLink className="navlink" to="/search"><i class="fas fa-search"></i></NavLink>
+            <NavLink className="navlink" to="/cart"><i class="fas fa-shopping-bag"></i></NavLink>
+       </div>
+          <Route path="/home" Component={Home}/>
       </div>
     );
   }
--- apple-nav/src/components/Home/Home.js
+++ apple-nav/src/components/Home/Home.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const Home = props => {
+class Home extends Component {
+
+    render(){
   return(
     <div className="home">
-        <h1>iPhone X</h1>
-        <h4>Say hello to the future</h4>
-        <img src="./images/iphonex.jpg" alt="iphone pic"/>
+        <div><h1>iPhone X</h1></div>
+        <div><h4>Say hello to the future</h4></div> 
+        <div><img src="./images/iphonex.jpg" alt="iphone pic"/></div>
+        
     </div>
   );
+}
 };
 
 export default Home;
