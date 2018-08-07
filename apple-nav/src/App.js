da6fd873a0579cedb2e1ebaf793739cd3cfb62e6
sca4cs <shannon.atkinson.ca@gmail.com>
2018-08-07 16:06:23 -0400

    project setup

---
+++ apple-nav/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import { NavLink, Route } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
