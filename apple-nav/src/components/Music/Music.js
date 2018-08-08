81c0ced5ae9aad95b28ec7a2c0b381a566516886
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-08 16:11:32 -0700

    Major changes - Added icons to submenus
    Added icons to Mac, TV and Music submenus. Created img folders, screenshotted the icons from the apple's website, imported icons to its corresponding components, added icons to the arrays and mapped through the icons.

--- apple-nav/src/components/Music/Music.js
+++ apple-nav/src/components/Music/Music.js
@@ -8,13 +8,46 @@ import {
     NavLink
   } from "react-router-dom";
 
+
+//IMPORTING IMAGES FROM IMG FOLDER
+import applemusic from './img/apple_music.png';
+import itunes from './img/itunes.png';
+import homepod from './img/homepod.png';
+import ipodtouch from './img/ipod_touch.png';
+import musicaccesories from './img/music_accesories.png';
+import giftcards from './img/gift_cards.png';
+
 const musicItems = [
-    {item: 'Apple Music', id: 1},
-    {item: 'iTunes', id: 2},
-    {item: 'HomePod', id: 3},
-    {item: 'iPod Touch', id: 4},
-    {item: 'Music Accesories', id: 5},
-    {item: 'Gift Cards', id: 6}
+    {
+        item: 'Apple Music',
+        id: 1,
+        icon: applemusic,
+    },
+    {
+        item: 'iTunes', 
+        id: 2,
+        icon: itunes,
+    },
+    {
+        item: 'HomePod', 
+        id: 3,
+        icon: homepod,
+    },
+    {
+        item: 'iPod Touch', 
+        id: 4,
+        icon: ipodtouch,
+    },
+    {
+        item: 'Music Accesories', 
+        id: 5,
+        icon: musicaccesories,
+    },
+    {
+        item: 'Gift Cards', 
+        id: 6,
+        icon: giftcards,
+    }
 ]
 
 class Music extends Component {
@@ -27,7 +60,12 @@ class Music extends Component {
                 <ul>
                     {musicItems.map(item => (
                         <li key={item.id}>
-                            <Link to={"/"} className="music-link">{item.item}</Link>
+                            <Link to={"/"} className="music-link">
+                                <div className="icon-container">
+                                    <img src={item.icon} alt='icon'/><br/>
+                                </div>
+                                {item.item}
+                            </Link>
                         </li>
                     ))}
                 </ul>
--- apple-nav/src/components/TV/TV.js
+++ apple-nav/src/components/TV/TV.js
@@ -8,12 +8,18 @@ import {
     NavLink
   } from "react-router-dom";
 
+//IMPORTING IMAGES FROM IMG FOLDER
+import tvicon from './img/tv.png';
+import tvappicon from './img/tv_app.png';
+import accesoriesicon from './img/accesories.png';
+import compareicon from './img/compare.png';
+
 const tvItems = [
-    {item: 'Apple TV 4K', id: 1},
-    {item: 'Apple TV', id: 2},
-    {item: 'TV App', id: 3},
-    {item: 'Accesories', id: 4},
-    {item: 'Compare', id: 5}
+    {item: 'Apple TV 4K', id: 1, icon: tvicon},
+    {item: 'Apple TV', id: 2, icon: tvicon},
+    {item: 'TV App', id: 3, icon: tvappicon},
+    {item: 'Accesories', id: 4, icon: accesoriesicon},
+    {item: 'Compare', id: 5, icon: compareicon}
 ]
 
 class Tv extends Component {
@@ -24,9 +30,14 @@ class Tv extends Component {
         return (
             <div className="tv-nav-container">
                 <ul>
-                    {tvItems.map(tv => (
-                        <li key={tv.id}>
-                            <Link to={"/"} className="tv-link">{tv.item}</Link>
+                    {tvItems.map(item => (
+                        <li key={item.id}>
+                            <Link to={"/"} className="tv-link">
+                                <div className="icon-container">
+                                    <img src={item.icon} alt='icon'/><br/>
+                                </div>
+                                {item.item}
+                            </Link>
                         </li>
                     ))}
                 </ul>
