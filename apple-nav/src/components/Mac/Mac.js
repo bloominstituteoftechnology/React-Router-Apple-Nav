e488b2103e7c0b706e212468855f678748b7cf96
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-07 18:52:40 -0700

    Major changes - Mac Submenu
    Displaying array of items inside the Mac navlink.

--- apple-nav/src/components/Mac/Mac.js
+++ apple-nav/src/components/Mac/Mac.js
@@ -1,4 +1,25 @@
 import React, { Component } from 'react';
+import './Mac.css';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    NavLink
+  } from "react-router-dom";
+
+const macItems = [
+    {item: 'MacBook', id: 1},
+    {item: 'MacBook Air', id: 2},
+    {item: 'MacBook Pro', id: 3},
+    {item: 'iMac', id: 4},
+    {item: 'iMac Pro', id: 5},
+    {item: 'Mac Pro', id: 6},
+    {item: 'Mac mini', id: 7},
+    {item: 'Accesories', id: 8},
+    {item: 'High Sierra', id: 9},
+    {item: 'Compare', id: 10}
+]
 
 class Mac extends Component {
     constructor() {
@@ -6,8 +27,15 @@ class Mac extends Component {
     }
     render() {
         return (
-            <div>
+            <div className="mac-container">
                 This is Mac
+                <ul>
+                    {macItems.map(mac => (
+                        <li key={mac.id}>
+                            <Link to={"/"}>{mac.item}</Link>
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
