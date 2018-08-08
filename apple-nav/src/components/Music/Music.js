b4d234489f706e2cb05335e82a8bdd750069495c
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-07 20:47:36 -0700

    Major changes - Music submenu
    Finished setting up the music menu. Also, finished MVP. Working on setting up the icons."

--- apple-nav/src/components/Music/Music.js
+++ apple-nav/src/components/Music/Music.js
@@ -1,4 +1,21 @@
 import React, { Component } from 'react';
+import './Music.css';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    NavLink
+  } from "react-router-dom";
+
+const musicItems = [
+    {item: 'Apple Music', id: 1},
+    {item: 'iTunes', id: 2},
+    {item: 'HomePod', id: 3},
+    {item: 'iPod Touch', id: 4},
+    {item: 'Music Accesories', id: 5},
+    {item: 'Gift Cards', id: 6}
+]
 
 class Music extends Component {
     constructor() {
@@ -6,8 +23,14 @@ class Music extends Component {
     }
     render() {
         return (
-            <div>
-                This is music
+            <div className="music-nav-container">
+                <ul>
+                    {musicItems.map(item => (
+                        <li key={item.id}>
+                            <Link to={"/"} className="music-link">{item.item}</Link>
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
