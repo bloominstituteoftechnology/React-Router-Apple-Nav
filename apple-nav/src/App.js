cb77f8e1854a989fc75edb9505004716f36f9d3f
jaymzsocool <james.mcraejr@gmail.com>
2018-08-08 18:31:24 -0400

    able to display subnav

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import NavWrap from './components/NavWrap';
 import { Route } from 'react-router-dom';
-
 import {mainLink} from './data';
+import SubNav from './components/SubNav'
 
 class App extends Component {
-  render() { console.log(mainLink.subLink)
-    return (
-      
-      <Route path = '/' render = {props => <NavWrap {...props} link = {mainLink} />} />      
-      
+  render() { 
+    return (     
+      <div> 
+      <Route path = '/' render = {props => <NavWrap {...props} link = {mainLink} />} />         
+      <Route path = '/:id' render = {props => <SubNav {...props} link = {mainLink[props.match.params.id - 1].subLink} />} />
+      </div>     
     );
   }
 }
--- apple-nav/src/components/SubNav.js
+++ apple-nav/src/components/SubNav.js
@@ -7,7 +7,7 @@ const SubNav = (props) => {
     
         return(
             <div className = "sub-nav">
-                {props.link.subLink.map(data => (
+                {props.link.map(data => (
                    <div>
                     <img src = {data.icon} alt = {data.name} />
                     <span>{data.name}</span>
