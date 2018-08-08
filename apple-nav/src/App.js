aaebb7d66b276b755ce218a69e469dace63e310a
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-08-07 18:02:08 -0600

    Basic functionality of nav working

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavHeader from './Components/NavHeader';
+import {Route} from 'react-router-dom'; 
+import SubNav from './Components/SubNav'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <NavHeader />
+        <Route exact path = "/" component = {Home}/>
+        <Route path = "/:name" component = {SubNav}  />
       </div>
     );
   }
+}
+
+const Home = () => {
+  return(
+    <div>
+    <h1>Home Page</h1>
+  </div>
+  )
+  
 }
 
 export default App;
---
+++ apple-nav/src/Components/NavHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import Nav from './Nav'; 
+
+const navItems = ["Mac", "Ipad", "Iphone", "Watch", "TV", "Music", "Support" ]
+
+const NavHeader = props => {
+    return(
+        <div className = "nav-main">
+          {navItems.map((item, index) =>  <Nav key = {index} name = {item} /> )}
+        </div>
+    )
+}
+
+export default NavHeader; 
