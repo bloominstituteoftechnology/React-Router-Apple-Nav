336505107891860d8e2ffb1d3aaad19bc580e65e
vanceleon <vanceleon44@gmail.com>
2018-08-14 16:10:18 -0400

    added wrapper and route

---
+++ apple-nav/src/components/NavWrapper.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+
+class NavWrapper extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+    
+    render(){
+        return(
+            <div>testing</div>
+        )
+    }
+
+}
+
+export default NavWrapper;
