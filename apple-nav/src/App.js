4bd040f35334f0d37e852c84a3a3770f306079ed
Arthur Bates <aabatesj@gmail.com>
2018-08-14 17:10:58 -0400

    nav components
    Basic nav displaying with nav items

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import NavWrapper from './components/NavWrapper';
+import { Route } from 'react-router-dom';
+import SubNav from './components/SubNav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavWrapper />
+        <Route exact path="/" component={Home} />
+        <Route path="/:name" component={SubNav} />
       </div>
     );
   }
 }
 
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
 export default App;
