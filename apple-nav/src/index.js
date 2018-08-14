86b8584b9b9e7d8eff52b8acc0d1c500534279a7
Cwright0428 <cwright0428@yahoo.com>
2018-08-14 15:04:05 -0500

    wrapped App component in router

--- apple-nav/src/index.js
+++ apple-nav/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Link, NavLink} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    rootElement
+);
