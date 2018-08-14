ded57f7c84d97b13deb733c8bb3cdaa55cfb944d
AI2EJAY <JHammett1314@gmail.com>
2018-08-14 15:06:12 -0500

    Setup initial react-router stuff

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route, Link, NavLink} from 'react-router-dom';
+
+import NavWrapper from './components/NavWrapper'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Route path="/" component={NavWrapper}/>
+       <h1>React Router 2</h1>
       </div>
     );
   }
