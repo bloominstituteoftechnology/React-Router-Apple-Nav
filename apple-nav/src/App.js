da998ad3df5efbd42ce4e7bde6bf1d5f2af8a1ec
JoCatherine Griggs <jocafrin@gmail.com>
2018-08-14 14:42:28 -0400

    Created menuData array

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from "react-router-dom";
 import './App.css';
+
+const menuData = [
+  {name: 'Mac',
+  id: 1,
+  products: ['MacBook', 'MacBook Air', 'MacBook Pro', 'iMac', 'iMac Pro', 'Mac Pro', 'Mac mini']},
+  {name: 'iPad',
+  id: 2,
+  products: ['iPad Pro', 'iPad', 'iPad mini 4', 'Accessories']},
+  {name: 'iPhone',
+  id: 3,
+  products: ['iPhone X', 'iPhone 8', 'iPhone 7', 'iPhone 6s', 'iPhone SE', 'Accessories']},
+  {name: 'Watch',
+  id: 4,
+  products: ['Apple Watch Series 1', 'Apple Watch Series 3', 'Apple Watch Nike+', 'Apple Watch Hermes', 'Apple Watch Edition', 'Bands', 'Accessories' ]},
+  {name: 'TV',
+  id: 5,
+  products: ['Apple TV', 'Apple TV 4K',  'TV App']},
+  {name: 'Music',
+  id: 6,
+  products: ['iPod touch', 'Apple Music', 'iTunes', 'HomePod' ]}
+]
+
+
+
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
