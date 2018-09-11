b562a2f274d43dd64dbb6a2698319d6b14af21d2
Michael Agard <mdagard@gmail.com>
2018-09-11 18:46:53 -0400

    added NavLink to div

--- apple-nav/src/components/NavOptions/TV.js
+++ apple-nav/src/components/NavOptions/TV.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 // import './NavBar.css';
 
 class TV extends Component {
   render() {
     return (
       <div className="tv-options">
-        <div>tv-options button</div>
+        <NavLink to="/mac">mac</NavLink>
       </div>
     );
   }
