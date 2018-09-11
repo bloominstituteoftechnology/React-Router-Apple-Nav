abcd2d04f38d8439afe00d7eaf0f70ce261c4ef5
Michael Agard <mdagard@gmail.com>
2018-09-11 16:41:12 -0400

    added logo adjusted nav bar styling

--- apple-nav/src/components/NavBar/NavBar.js
+++ apple-nav/src/components/NavBar/NavBar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
+import Logo from './logo.png';
 export default class NavBar extends Component {
   render() {
     return (
       <div className="nav-bar">
+        <img className="logo" alt="apple logo" src={Logo} />
         <NavLink
           to="/mac"
           activeStyle={{
