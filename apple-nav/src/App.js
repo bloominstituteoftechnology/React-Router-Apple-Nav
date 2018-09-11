8d3ff09d8a7a0ada766ae910b636b4f80c3f2538
decagondev <decagondevelopment@gmail.com>
2018-09-11 22:23:20 +0100

    (STRETCH STYLES) : atarted wrapper styles

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import NavWrapper from "./components/NavWrapper";
 import SubNav from "./components/SubNav";
+import "./App.css"
 
 class App extends Component {
   state = {
@@ -29,7 +30,7 @@ class App extends Component {
   };
   render() {
     return (
-      <div className="App">
+      <div className="wrapper">
         <NavWrapper links={this.state.links} />
         <Route path="/:name" render={props => <SubNav {...props} />} />
       </div>
