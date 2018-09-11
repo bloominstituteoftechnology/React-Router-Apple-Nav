6a174875c45c56ca1ebc7522b809074ac0312ceb
decagondev <decagondevelopment@gmail.com>
2018-09-11 22:09:19 +0100

    (STRETCH PROJECT) added iPhone images

--- apple-nav/src/components/SubNav/index.jsx
+++ apple-nav/src/components/SubNav/index.jsx
@@ -33,18 +33,16 @@ const links = [
     name: "iPhone",
     image: "apple.svg",
     subNav: [
-      { name: "MacBook", image: "images/mac/macbook_dark_large.svg" },
-      { name: "MacBook Air", image: "images/mac/macbookair_dark_large.svg" },
-      { name: "MacBook Pro", image: "images/mac/macbookpro_dark_large.svg" },
-      { name: "iMac", image: "images/mac/imac_dark_large.svg" },
-      { name: "iMac Pro", image: "images/mac/imac_pro_dark_large.svg" },
-      { name: "Mac Pro", image: "images/mac/macpro_dark_large.svg" },
-      { name: "Mac mini", image: "images/mac/macmini_dark_large.svg" },
-      { name: "Accessories", image: "images/mac/mac_acc_dark_large.svg" },
-      { name: "High Sierra", image: "images/mac/mac_osx_dark_large.svg" },
-      { name: "Compare", image: "images/mac/mac_comp_dark_large.svg" }
+      { name: "iPhone X", image: "images/iphone/iphonex_large.svg" },
+      { name: "iPhone 8", image: "images/iphone/iphone8_large.svg" },
+      { name: "iPhone 7", image: "images/iphone/iphone7_large.svg" },
+      { name: "iPhone 6s", image: "images/iphone/iphone6s_large.svg" },
+      { name: "iPhone SE", image: "images/iphone/iphonese_large.svg" },
+      { name: "iOS 11", image: "images/iphone/iphone_ios_large.svg" },
+      { name: "Accessories", image: "images/iphone/iphone_acc_large.svg" },
+      { name: "Compare", image: "images/iphone/iphone_comp_large.svg" }
     ]
-  },
+  },,
   {
     name: "Watch",
     image: "apple.svg",
