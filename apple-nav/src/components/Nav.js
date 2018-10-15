373a6fd00804ed4042aba1db4dd042b5d2d3daab
kyranmccann <kyran.mccann@gmail.com>
2018-10-15 15:41:26 -0700

    initial commit

---
+++ apple-nav/src/components/Nav.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+
+export default Nav;
---
+++ apple-nav/src/components/NavWrapper.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Nav from './Nav';
+
+
+export default NavWrapper;
