3d8db0f98ef18f5db2a8fd4fbd7b8e9195cd8992
Rahul Desai <rd825@georgetown.edu>
2018-10-15 23:40:54 -0400

    Got animations to work

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import posed, { PoseGroup } from 'react-pose';
+
 import './App.css';
+
 import TopNav from './components/TopNav';
 import Home from './components/Home';
 import Mac from './components/products/Mac';
@@ -10,18 +13,29 @@ import Watch from './components/products/Watch';
 import TV from './components/products/TV';
 import Music from './components/products/Music';
 
+const RoutesContainer = posed.div({
+  enter: {x: 0},
+  exit: {x: 200}
+});
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopNav />
-        <Route exact path="/" component={Home}/>
-        <Route path="/mac" component={Mac}/>
-        <Route path="/ipad" component={IPad}/>
-        <Route path="/iphone" component={IPhone}/>
-        <Route path="/watch" component={Watch}/>
-        <Route path="/tv" component={TV}/>
-        <Route path="/music" component={Music}/>
+        <PoseGroup>
+          <RoutesContainer key={Date.now()}>
+            <Switch location={window.location}>
+              <Route exact path="/" component={Home}/>
+              <Route path="/mac" component={Mac}/>
+              <Route path="/ipad" component={IPad}/>
+              <Route path="/iphone" component={IPhone}/>
+              <Route path="/watch" component={Watch}/>
+              <Route path="/tv" component={TV}/>
+              <Route path="/music" component={Music}/>
+            </Switch>
+          </RoutesContainer>
+        </PoseGroup>
       </div>
     );
   }
