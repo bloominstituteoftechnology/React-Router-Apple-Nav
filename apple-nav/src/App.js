657ebf9103a55a5a94d65ade417fc37fbaa7f371
Brad Mortensen <b.mort86@gmail.com>
2018-10-16 15:45:33 -0600

    building apple nav components

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import styled from 'styled-components';
+import MainNavContainer from './Components/MainNav/MainNavContainer'
+
+const AppContainer = styled.div`
+  font-size: 62.5%;
+  `;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppContainer className="App">
+        <Route render={() => <MainNavContainer />} />
+      </AppContainer>
     );
   }
 }
---
+++ apple-nav/src/Components/MainNav/MainNavContainer.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components'
+
+const HeaderContainer = styled.div `
+  height: 60px;
+  width: 100%;
+  background-color: #393939;
+`;
+
+const NavContainer = styled.div`
+  margin: 0 auto;
+  max-width: 900px;
+  display: flex;
+  flex-direction: row;
+  justify-content:space-around;
+  align-items: center;
+  height: 100%;
+`;
+
+const NavText = styled.p`
+  text-decoration: none;
+  color: white;
+  font-size: 1.2rem;
+  font-family: "SF Pro Display";
+`;
+
+class MainNavContainer extends Component{
+  constructor() {
+    super();
+    this.state= {
+
+    };
+  }  
+  render() {
+    return (
+      <HeaderContainer className="main-nav-container">
+        <NavContainer className="nav-div">
+          <NavText to="/mac" >Mac</NavText>
+          <NavText to="/ipad" >iPad</NavText>
+          <NavText to="/iphone" >iPhone</NavText>
+          <NavText to="/watch" >Watch</NavText>
+          <NavText to="/tv" >TV</NavText>
+          <NavText to="/music" >Music</NavText>
+          <NavText to="/support" >Support</NavText>   
+        </NavContainer>       
+      </HeaderContainer>
+    );
+  }
+}
+
+
+export default MainNavContainer;
