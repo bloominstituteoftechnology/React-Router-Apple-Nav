065b84edcb61340a35bc90a620bb4c902ddfa2e5
Ryan Boris <neodynia@icloud.com>
2018-10-16 07:03:29 -0400

    Creates each subnav tab category

---
+++ apple-nav/src/components/Mac.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import macData from '../data/macData';
+import Product from './Product';
+
+const Mac = () => {
+    return (
+        <div className="subnav mac">
+            <div className="maccontainer">
+                {macData.map((product,i) => <Product key={i} product={product} />)}
+            </div>
+        </div>
+    )
+}
+
+export default Mac;
