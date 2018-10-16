ce2e81092007c5458d82975e0cc545530e3e38d2
MEBonham <programmer@mckaybonham.com>
2018-10-16 15:02:50 -0600

    Broke App into a header and a default filler panel

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import NavWrapper from "./components/NavWrapper";
+import Filler from "./components/Filler";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <section className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </section>
+        <NavWrapper />
+        <Filler />
       </div>
     );
   }
