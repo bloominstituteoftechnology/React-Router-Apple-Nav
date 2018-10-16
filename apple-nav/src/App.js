26e9b2b1e1a4dd42d4f3db01397c4b4f708d9523
KHollobaugh <khollobaugh@gmail.com>
2018-10-16 16:55:29 -0400

    Added component structure

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import {TopNavigation} from './Components/TopNavigation';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      topData: [],
+      bottomData: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
-
+        <Route
+          path="/"
+          render={props => <TopNavigation {...props} />}
+        />
       </div>
     );
   }
