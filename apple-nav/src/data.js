94c391eeca22222869c8c4bfa0137fa9a67c669f
ssmodish <smodish@hotmail.com>
2018-10-16 18:13:39 -0400

    building dataset - mac ipad iphone complete

--- apple-nav/src/data.js
+++ apple-nav/src/data.js
@@ -2,94 +2,151 @@ export default [
   {
     id: 000,
     name: 'Apple Logo',
+    items: [],
+  },
+  {
+    id: 001,
+    name: 'Mac',
     items: [
       {
         id: 100,
         name: 'MacBook'
-      }
+      },
       {
         id: 101,
         name: 'MacBook Air'
-      }
+      },
       {
         id: 102,
         name: 'MacBook Pro'
-      }
+      },
       {
         id: 103,
         name: 'iMac'
-      }
+      },
       {
         id: 104,
         name: 'iMac Pro'
-      }
+      },
       {
         id: 105,
         name: 'Mac Pro'
-      }
+      },
       {
         id: 106,
         name: 'Mac mini'
-      }
+      },
       {
         id: 107,
         name: 'Accessories'
-      }
+      },
       {
         id: 108,
         name: 'Mojave'
-      }
+      },
       {
         id: 109,
         name: 'Compare'
-      }
-
+      },
     ]
-  }
-  {
-    id: 001,
-    name: 'Mac',
-    items: []
-  }
+  },
   {
     id: 002,
     name: 'iPad',
-    items: []
-  }
+    items: [
+      {
+        id: 200,
+        name: 'iPad Pro'
+      },
+      {
+        id: 201,
+        name: 'iPad'
+      },
+      {
+        id: 202,
+        name: 'iPad mini 4'
+      },
+      {
+        id: 203,
+        name: 'iOS 12'
+      },
+      {
+        id: 204,
+        name: 'Accessories'
+      },
+      {
+        id: 205,
+        name: 'Compare'
+      },
+    ]
+  },
   {
     id: 003,
     name: 'iPhone',
-    items: []
-  }
+    items: [
+      {
+        id: 300,
+        name: 'iPhone XS'
+      },
+      {
+        id: 301,
+        name: 'iPhone XR'
+      },
+      {
+        id: 302,
+        name: 'iPhone 8'
+      },
+      {
+        id: 303,
+        name: 'iPhone 7'
+      },
+      {
+        id: 304,
+        name: 'iOS 12'
+      },
+      {
+        id: 305,
+        name: 'AirPods'
+      },
+      {
+        id: 306,
+        name: 'Accessories'
+      },
+      {
+        id: 307,
+        name: 'Compare'
+      },
+    ]
+  },
   {
     id: 004,
     name: 'Watch',
     items: []
-  }
+  },
   {
     id: 005,
     name: 'TV',
     items: []
-  }
+  },
   {
     id: 006,
     name: 'Music',
     items: []
-  }
+  },
   {
     id: 007,
     name: 'Support',
     items: []
-  }
+  },
   {
     id: 008,
     name: 'Search',
     items: []
-  }
+  },
   {
     id: 009,
     name: 'Shopping Bag',
     items: []
-  }
+  },
 ]
 
