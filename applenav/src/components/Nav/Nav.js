e6e67717380f57f01770ef7094dffc0d1b46e400
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-10-16 12:49:37 -0700

    basic nav

---
+++ applenav/src/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Nav";
+
+const Nav = props => {
+  return (
+    <div className="nav-items">
+      <a href="#">Home</a>
+      <a href="#">iPad</a>
+      <a href="#">iPhone</a>
+      <a href="#">Watch</a>
+      <a href="#">TV</a>
+      <a href="#">Music</a>
+      <a href="#">Support</a>
+    </div>
+  );
+};
+
+export default Nav;
