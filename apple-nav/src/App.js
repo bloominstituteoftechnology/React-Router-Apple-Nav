80714aa1e9219a59656ee92aa3baf421fa01cda0
Hargobind Atwal <ethaipath@gmail.com>
2018-10-16 15:45:00 -0700

    Init commit

---
+++ apple-nav/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import data from './data';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
