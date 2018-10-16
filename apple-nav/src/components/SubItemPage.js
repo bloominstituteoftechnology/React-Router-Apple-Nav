14b6fc467bab361b7808b1d3fdda4b81039380d5
kyranmccann <kyran.mccann@gmail.com>
2018-10-16 14:06:40 -0700

    removing console.logs

--- apple-nav/src/components/SubItemPage.js
+++ apple-nav/src/components/SubItemPage.js
@@ -1,20 +1,16 @@
 import React from 'react';
 
 const SubItemPage = props => {
-  console.log('subpage');
   let items = [];
   props.link.map(linkObj => (
     linkObj.subLinks.map(subLink => (
       items.push(subLink)
     ))
   ));
-  console.log(items);
   const item =  items.find(thing => `${thing.name}` === props.match.params.subName);
   const parent = props.link.find(thing => `${thing.name}` === props.match.params.name);
   let parentName = '';
   parentName = parent.name;
-  console.log('are you my mother', parentName);
-  console.log('parent', props.match.params.name);
   let dark = ( parentName === 'Mac' || parentName === 'TV');
   let classes = 'sub-item-page ';
   if (!dark) {
