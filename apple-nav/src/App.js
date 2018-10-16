373b44c1d56b08637254ec335cb781194bef7b86
TRIF3X <thewizardofcrypto@gmail.com>
2018-10-16 14:40:04 -0500

    enviornment set up

---
+++ apple-nav/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to Apple-nav!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
