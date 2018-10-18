306265ae4b224389bf9e186745bc09841d56bb91
TheGameFreak720 <luis.alvarez@commithub.com>
2018-10-17 23:45:35 -0400

    added ipad images and added the links necessary to get it running

--- src/components/Nav/Ipad.js
+++ src/components/Nav/Ipad.js
@@ -4,34 +4,22 @@ const Ipad = props => {
     return (
         <div>
             <div>
-                <img src={require('')} />
+                <img src={require('../../img/ipad/ipadpro.png')} />
             </div>
             <div>
-                <img src={require('')} />
+                <img src={require('../../img/ipad/ipad.png')} />
             </div>
             <div>
-                <img src={require('')} />
+                <img src={require('../../img/ipad/ipadmini4.png')} />
             </div>
             <div>
-                <img src={require('')} />
+                <img src={require('../../img/ipad/ios.png')} />
             </div>
             <div>
-                <img src={require('')} />
+                <img src={require('../../img/ipad/accesories.png')} />
             </div>
             <div>
-                <img src={require('')} />
-            </div>
-            <div>
-                <img src={require('')} />
-            </div>
-            <div>
-                <img src={require('')} />
-            </div>
-            <div>
-                <img src={require('')} />
-            </div>
-            <div>
-                <img src={require('')} />
+                <img src={require('../../img/ipad/compare.png')} />
             </div>
         </div>
     );
