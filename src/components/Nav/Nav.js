5cf5b1e82a2663b334f6a17b323d44df37afba79
TheGameFreak720 <luis.alvarez@commithub.com>
2018-10-18 20:41:46 -0400

    nav is styled

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FlexContainerBlack } from './Styles/Index';
 
 const Nav = props => {
     return (
         <div>
-            <nav>
-                <Link to='/mac'>Mac</Link>
-                <Link to='/ipad'>iPad</Link>
-                <Link to='/iphone'>iPhone</Link>
-                <Link to='/watch'>Watch</Link>
-                <Link to='/tv'>TV</Link>
-                <Link to='/music'>Music</Link>
-            </nav>
+            <FlexContainerBlack>
+                <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>Home</Link>
+                <Link to='/mac' style={{ textDecoration: 'none', color: 'white' }}>Mac</Link>
+                <Link to='/ipad' style={{ textDecoration: 'none', color: 'white' }}>iPad</Link>
+                <Link to='/iphone' style={{ textDecoration: 'none', color: 'white' }}>iPhone</Link>
+                <Link to='/watch' style={{ textDecoration: 'none', color: 'white' }}>Watch</Link>
+                <Link to='/tv' style={{ textDecoration: 'none', color: 'white' }}>TV</Link>
+                <Link to='/music' style={{ textDecoration: 'none', color: 'white' }}>Music</Link>
+            </FlexContainerBlack>
         </div>
     );
 };
---
+++ src/components/Nav/Styles/Index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const FlexContainerBlack = styled.section `
+    display: flex;
+    justify-content: space-around;
+    background-color: black;
+`;
+
+export const FlexContainerWhite = styled.section `
+    display: flex;
+    justify-content: space-around;
+    background-color: white;
+`;
+
