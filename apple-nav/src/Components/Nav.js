0e38b999e6013efae4d60687418fb4daf78d5d46
Robin U Familara <robin.familara@cgius.com>
2018-10-20 09:21:02 -0500

    Sub-nav styled

--- apple-nav/src/Components/Nav.js
+++ apple-nav/src/Components/Nav.js
@@ -1,12 +1,12 @@
 import React from 'react'; 
 import { NavLink } from 'react-router-dom'; 
 
- const Nav = props => {
+const Nav = props => {
     return (
-        <div>
-            <NavLink to = {`/${props.name.toLowerCase()}`}>{props.name}</NavLink>
+        <div className = "nav-item">
+            <NavLink exact activeClassName = 'selected' to = {`/${props.name.toLowerCase()}`}>{props.name}</NavLink>
         </div>
     )
 }
 
- export default Nav;
+export default Nav;
