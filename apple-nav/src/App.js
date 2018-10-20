e7b097e357814d11061daea6c07084adcdae338c
jbrock1981 <jbrock1981@gmail.com>
2018-10-20 12:28:19 -0500

    completed mvp

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+
 import './App.css';
+import NavBar from './components/NavBar';
+import NavMenu from './components/NavMenu';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentTab: "none"
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Route path="/" component={NavBar} />
+        <Route path="/:id" component={NavMenu} />
+      </>
     );
   }
 }
