5feb8833bfcd1a00f12af02b825fd097364d6f75
iepoch <staclem@live.com>
2018-10-20 11:49:53 -0700

    Fixed issue with Images

--- apple-clone/src/apple-data.js
+++ apple-clone/src/apple-data.js
@@ -35,61 +35,61 @@ export const productData = [
         {
             id: 0,
             name: 'MacBook',
-            img: './images/macbook_dark_large.svg',
+            img: '../images/macbook_dark_large.svg',
             new: false
         },
         {
             id: 1,
             name: 'MacBook Air',
-            img: './images/macbookair_dark_large.svg',
+            img: '../images/macbookair_dark_large.svg',
             new: false
         },
         {
             id: 2,
             name: 'MacBook Pro',
-            img: './images/macbookpro_dark_large.svg',
+            img: '../images/macbookpro_dark_large.svg',
             new: true
         },
         {
             id: 3,
             name: 'iMac',
-            img: './images/imac_dark_large.svg',
+            img: '../images/imac_dark_large.svg',
             new: false
         },
         {
             id: 4,
             name: 'iMac Pro',
-            img: './images/imac_pro_dark_large.svg',
+            img: '../images/imac_pro_dark_large.svg',
             new: false
         },
         {
             id: 5,
             name: 'Mac Pro',
-            img: './images/macpro_dark_large.svg',
+            img: '../images/macpro_dark_large.svg',
             new: false
         },
         {
             id: 6,
             name: 'Mac Mini',
-            img: './images/macmini_dark_large.svg',
+            img: '../images/macmini_dark_large.svg',
             new: false
         },
         {
             id: 7,
             name: 'Accessories',
-            img: './images/mac_acc_dark_large.svg',
+            img: '../images/mac_acc_dark_large.svg',
             new: false
         },
         {
             id: 8,
             name: 'Majove',
-            img: './images/mac_osx_dark_large.svg',
+            img: '../images/mac_osx_dark_large.svg',
             new: false
         },
         {
             id: 9,
             name: 'Compare',
-            img: './images/mac_comp_dark_large.svg',
+            img: '../images/mac_comp_dark_large.svg',
             new: false
         }
     ],
@@ -97,37 +97,37 @@ export const productData = [
         {
             id: 0,
             name: 'iPad Pro',
-            img: './images/ipadpro_light_large.svg',
+            img: '../images/ipadpro_light_large.svg',
             new: false
         },
         {
             id: 1,
             name: 'iPad',
-            img: './images/ipad_large.svg',
+            img: '../images/ipad_large.svg',
             new: true
         },
         {
             id: 2,
             name: 'iPad mini 4',
-            img: './images/ipadmini_large.svg',
+            img: '../images/ipadmini_large.svg',
             new: false
         },
         {
             id: 3,
             name: 'iOS 12',
-            img: './images/ipad_ios10_large.svg',
+            img: '../images/ipad_ios10_large.svg',
             new: false
         },
         {
             id: 4,
             name: 'Accessories',
-            img: './images/ipad_acc_large.svg',
+            img: '../images/ipad_acc_large.svg',
             new: false
         },
         {
             id: 5,
             name: 'Compare',
-            img: './images/ipad_comp_large.svg',
+            img: '../images/ipad_comp_large.svg',
             new: false
         }
     ],
@@ -135,49 +135,49 @@ export const productData = [
         {
             id: 0,
             name: 'iPhone Xs',
-            img: './images/iphonexs_dark_large.svg',
+            img: '../images/iphonexs_dark_large.svg',
             new: true
         },
         {
             id: 1,
             name: 'iPhone Xr',
-            img: './images/iphonexr_dark_large.svg',
+            img: '../images/iphonexr_dark_large.svg',
             new: true
         },
         {
             id: 2,
             name: 'iPhone 8',
-            img: './images/iphone8_dark_large.svg',
+            img: '../images/iphone8_dark_large.svg',
             new: false
         },
         {
             id: 3,
             name: 'iPhone 7',
-            img: './images/iphone7_dark_large.svg',
+            img: '../images/iphone7_dark_large.svg',
             new: false
         },
         {
             id: 4,
             name: 'iOS 12',
-            img: './images/ios_dark_large.svg',
+            img: '../images/ios_dark_large.svg',
             new: false
         },
         {
             id: 5,
             name: 'AirPods',
-            img: './images/airpods_dark_large.svg',
+            img: '../images/airpods_dark_large.svg',
             new: false
         },
         {
             id: 6,
             name: 'Accessories',
-            img: './images/accessories_dark_large.svg',
+            img: '../images/accessories_dark_large.svg',
             new: false
         },
         {
             id: 7,
             name: 'Compare',
-            img: './images/compare_dark_large.svg',
+            img: '../images/compare_dark_large.svg',
             new: false
         }
     ],
@@ -185,49 +185,49 @@ export const productData = [
         {
             id: 0,
             name: 'Apple Watch Series 4',
-            img: './images/watch_nav_series_2_large.svg',
+            img: '../images/watch_nav_series_2_large.svg',
             new: true
         },
         {
             id: 1,
             name: 'Apple Watch Nike+',
-            img: './images/watch_nav_nike_large.svg',
+            img: '../images/watch_nav_nike_large.svg',
             new: true
         },
         {
             id: 2,
             name: 'Apple Watch Hermès',
-            img: './images/watch_nav_hermes_large.svg',
+            img: '../images/watch_nav_hermes_large.svg',
             new: true
         },
         {
             id: 3,
             name: 'Apple Watch Series 3',
-            img: './images/watch_nav_edition_large.svg',
+            img: '../images/watch_nav_edition_large.svg',
             new: false
         },
         {
             id: 4,
             name: 'WatchOS',
-            img: './images/watch_nav_watch_os_large.svg',
+            img: '../images/watch_nav_watch_os_large.svg',
             new: false
         },
         {
             id: 5,
             name: 'Bands',
-            img: './images/watch_nav_bands_large.svg',
+            img: '../images/watch_nav_bands_large.svg',
             new: false
         },
         {
             id: 6,
             name: 'Accessories',
-            img: './images/watch_nav_accessories_large.svg',
+            img: '../images/watch_nav_accessories_large.svg',
             new: false
         },
         {
             id: 7,
             name: 'Compare',
-            img: './images/watch_nav_compare_large.svg',
+            img: '../images/watch_nav_compare_large.svg',
             new: false
         }
     ],
@@ -235,31 +235,31 @@ export const productData = [
         {
             id: 0,
             name: 'Apple TV 4K',
-            img: './images/apple_tv_dark_large.svg',
+            img: '../images/apple_tv_dark_large.svg',
             new: false
         },
         {
             id: 1,
             name: 'Apple TV',
-            img: './images/apple_tv_dark_large.svg',
+            img: '../images/apple_tv_dark_large.svg',
             new: false
         },
         {
             id: 2,
             name: 'TV App',
-            img: './images/tvapp_dark_large.svg',
+            img: '../images/tvapp_dark_large.svg',
             new: false
         },
         {
             id: 3,
             name: 'Accessories',
-            img: './images/tv_accessories_dark_large.svg',
+            img: '../images/tv_accessories_dark_large.svg',
             new: false
         },
         {
             id: 4,
             name: 'Compare',
-            img: './images/tv_compare_dark_large.svg',
+            img: '../images/tv_compare_dark_large.svg',
             new: false
         }
     ],
@@ -267,43 +267,43 @@ export const productData = [
         {
             id: 0,
             name: 'Apple Music',
-            img: './images/apple_music_large.svg',
+            img: '../images/apple_music_large.svg',
             new: false
         },
         {
             id: 1,
             name: 'iTunes',
-            img: './images/itunes_large.svg',
+            img: '../images/itunes_large.svg',
             new: false
         },
         {
             id: 2,
             name: 'HomePod',
-            img: './images/homepod_icon_large.svg',
+            img: '../images/homepod_icon_large.svg',
             new: false
         },
         {
             id: 3,
             name: 'Airpods',
-            img: './images/airpods_light_large.svg',
+            img: '../images/airpods_light_large.svg',
             new: false
         },
         {
             id: 4,
             name: 'iPod Touch',
-            img: './images/ipod_touch_light_large.svg',
+            img: '../images/ipod_touch_light_large.svg',
             new: false
         },
         {
             id: 5,
             name: 'Accessories',
-            img: './images/music_accessories_large.svg',
+            img: '../images/music_accessories_large.svg',
             new: false
         },
         {
             id: 6,
             name: 'Gift Cards',
-            img: './images/gift_cards_large.svg',
+            img: '../images/gift_cards_large.svg',
             new: false
         }
     ]
--- apple-clone/src/components/SubNav.js
+++ apple-clone/src/components/SubNav.js
@@ -1,30 +1,48 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
-const ANav = styled.div `
-background: gray;
-heigh:48%;
-padding:10px;
-width:100%;
-`
+const ANav = styled.div`
+  display: flex;
+  align-items: center;
+  background: #141414;
+  color: #efefef;
+  heigh: 48%;
+  padding: 10px;
+  width: 100%;
+  text-decoration: none;
+`;
+const NavWrapper = styled.div`
+margin: 0 auto;
+  .subNav {
+    display: flex;
+    flex-direction: column;
+    text-decoration: none;
+  }
+  a {
+      color:white;
+    text-decoration: none;
+  }
+`;
 
-const SubNav= ({mac}) => {
+const TextSpan = styled.span`
+  text-align: center;
+  margin-top: 7px;
+`;
 
+const SubNav = ({ mac }) => {
   return (
     <ANav>
-          {mac.map(product =>
-              <NavLink to="/" className="subNav" key={product.id}>
-                  <img src={product.img} alt={product.name} />
-                  <span>{product.name}</span>
-     </NavLink>
-     )}
+      {mac.map(product => (
+        <NavWrapper>
+          <NavLink to="/" className="subNav" key={product.id}>
+            <img src={product.img} alt={product.name} />
+            <TextSpan>{product.name}</TextSpan>
+          </NavLink>
+        </NavWrapper>
+      ))}
+    </ANav>
+  );
+};
 
-  </ANav>
-  )
-
-}
-
-
-
-export default SubNav
+export default SubNav;
