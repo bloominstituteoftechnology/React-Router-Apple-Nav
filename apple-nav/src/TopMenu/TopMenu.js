3c9a93f2bb53ef808aed7ffc4c1b549976742f2a
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-10-20 20:50:01 -0700

    Created and styled top menu

--- apple-nav/src/TopMenu/TopMenu.js
+++ apple-nav/src/TopMenu/TopMenu.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const TopMenuContainer = styled.div`
+    box-sizing: border-box;
+    margin: 0 auto;
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #333333;
+`
+
+const Icon = styled.i`
+    color: white;
+`
+
+const TopMenuOption = styled.span`
+    color: white;
+`
 
 class TopMenu extends React.Component {
   render() {
     return (
-      <div>
-        <p>STUFF</p>
-      </div>
+      <TopMenuContainer>
+        <Icon className="fab fa-apple fa-2x"/>
+        <TopMenuOption>Mac</TopMenuOption>
+        <TopMenuOption>iPad</TopMenuOption>
+        <TopMenuOption>iPhone</TopMenuOption>
+        <TopMenuOption>Watch</TopMenuOption>
+        <TopMenuOption>TV</TopMenuOption>
+        <TopMenuOption>Music</TopMenuOption>
+        <TopMenuOption>Support</TopMenuOption>
+        <Icon className="fas fa-search fa-2x"/>
+        <Icon className="fas fa-shopping-bag fa-2x"/>
+      </TopMenuContainer>
     );
   }
 }
