1df5804c42af92d7312c555d0875c7e154fec71a
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-10-20 20:11:41 -0700

    Installed react router dependencies and created TopMenu component

---
+++ apple-nav/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TopMenu from './TopMenu/TopMenu.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TopMenu />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ apple-nav/src/TopMenu/TopMenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TopMenu extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>STUFF</p>
+      </div>
+    );
+  }
+}
+
+export default TopMenu;
