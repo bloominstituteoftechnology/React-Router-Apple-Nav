c21e320468ef543f06a93cf2b5fa446157788f14
paulapivat <paul.apivat@gmail.com>
2018-10-22 10:43:38 +0700

    add icons to TV and Watch Pages

--- src/components/Tv.js
+++ src/components/Tv.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import tv4kImg from '../img/tv/tv4k.png';
+import tvImg from '../img/tv/tv.png';
+import tvappImg from '../img/tv/tvapp.png';
+import tv_accesoriesImg from '../img/tv/tv_accesories.png';
+import tv_compareImg from '../img/tv/tv_compare.png';
 
 export default () => {
     return (
          <div>
             <h1>TV Page</h1>
             <p> This is the TV Page </p>
+            <div className="icon-row">
+                <img src={tv4kImg} alt="tv4k"/>
+                <img src={tvImg} alt="tv"/>
+                <img src={tvappImg} alt="tvapp"/>
+                <img src={tv_accesoriesImg} alt="tv_accesories"/>
+                <img src={tv_compareImg} alt="tv_compare"/>
+            </div>
         </div>
     );
 };
