e45145e328f43ae4e2c1dc59458ed8465097c4af
Denise Escobar <descobar1490@gmail.com>
2018-10-22 13:09:21 -0500

    Setting up my components and routes

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom'
 
-class App extends Component {
+import NavWrapper from './components/NavWrapper'
+import Nav from './components/Nav'
+
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Route exact path ='/' component={NavWrapper} />
+        <Route path = '/:category' component={Nav}  />
       </div>
     );
   }
 }
-
-export default App;
