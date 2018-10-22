a50dc334bf3a8a56c473be5f02b980ac88d2ffc3
paulapivat <paul.apivat@gmail.com>
2018-10-22 11:20:35 +0700

    use inline-styles on all Links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+
 import Mac from './components/Mac';
 import Ipad from './components/Ipad';
 import Iphone from './components/Iphone';
@@ -11,7 +12,6 @@ import Support from './components/Support';
 import './App.css';
 
 
-
 class App extends Component {
   render() {
     return (
@@ -19,12 +19,12 @@ class App extends Component {
         <nav className='navigation'>
             <div className="nav-button">Apple Logo</div>
             <Link to="/mac" style={{ textDecoration: 'none', color: 'white'}}>Mac</Link>
-            <Link to="/ipad">iPad</Link>
-            <Link to="/iphone">iPhone</Link>
-            <Link to="/watch">Watch</Link>
-            <Link to="/tv">TV</Link>
-            <Link to="/music">Music</Link>
-            <Link to="/support">Support</Link>
+            <Link to="/ipad" style={{ textDecoration: 'none', color: 'white'}}>iPad</Link>
+            <Link to="/iphone" style={{ textDecoration: 'none', color: 'white'}}>iPhone</Link>
+            <Link to="/watch" style={{ textDecoration: 'none', color: 'white'}}>Watch</Link>
+            <Link to="/tv" style={{ textDecoration: 'none', color: 'white'}}>TV</Link>
+            <Link to="/music" style={{ textDecoration: 'none', color: 'white'}}>Music</Link>
+            <Link to="/support" style={{ textDecoration: 'none', color: 'white'}}>Support</Link>
             <div>Search Icon</div>
             <div>Bag Icon</div>
         </nav>
