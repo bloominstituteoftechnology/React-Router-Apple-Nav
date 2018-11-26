32fa13797647281737ee2790c9b45c5b992b7ddc
Yasirah Krueng <ykrueng@MacBook-Pro.local>
2018-11-26 16:09:53 -0600

    initial commit

---
+++ apple-nav/src/index.js
@@ -0,0 +1,11 @@
+// Importing libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Importing components
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
