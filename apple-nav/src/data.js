44ba9d0ea413001d6fa9b2393dc5e3dd6c8bf1bf
Yasirah Krueng <ykrueng@MacBook-Pro.local>
2018-11-26 17:33:02 -0600

    add links data

---
+++ apple-nav/src/data.js
@@ -0,0 +1,248 @@
+const data = [
+  {
+    id: 0,
+    name: 'Home',
+    sublinks: []
+  },
+  {
+    id: 1,
+    name: 'Mac',
+    sublinks: [
+      {
+        id:0,
+        name: 'MacBook'
+      },
+      {
+        id:1,
+        name: 'MacBook Air'
+      },
+      {
+        id:2,
+        name: 'MacBook Pro'
+      },
+      {
+        id:3,
+        name: 'iMac'
+      },
+      {
+        id:4,
+        name: 'iMac Pro'
+      },
+      {
+        id:5,
+        name: 'Mac Pro'
+      },
+      {
+        id:6,
+        name: 'Mac mini'
+      },
+      {
+        id:7,
+        name: 'Accessories'
+      },
+      {
+        id:8,
+        name: 'Mojave'
+      },
+      {
+        id:9,
+        name: 'Compare'
+      },
+    ]
+  },
+  {
+    id: 2,
+    name: 'iPad',
+    sublinks: [
+      {
+        id:0,
+        name: 'iPad Pro'
+      },
+      {
+        id:1,
+        name: 'iPad Pro 10.5"'
+      },
+      {
+        id:2,
+        name: 'iPad 9.7"'
+      },
+      {
+        id:3,
+        name: 'iPad mini 4'
+      },
+      {
+        id:4,
+        name: 'iOS 12'
+      },
+      {
+        id:5,
+        name: 'Apple Pencil'
+      },
+      {
+        id:6,
+        name: 'Smart Keyboard'
+      },
+      {
+        id:7,
+        name: 'Accessories'
+      },
+      {
+        id:8,
+        name: 'Compare'
+      },
+    ]
+  },
+  {
+    id: 3,
+    name: 'iPhone',
+    sublinks: [
+      {
+        id: 0,
+        name: 'iPhone Xs'
+      },
+      {
+        id: 1,
+        name: 'iPhone XR'
+      },
+      {
+        id: 2,
+        name: 'iPhone 8'
+      },
+      {
+        id: 3,
+        name: 'iPhone 7'
+      },
+      {
+        id: 4,
+        name: 'iOS 12'
+      },
+      {
+        id: 5,
+        name: 'AirPods'
+      },
+      {
+        id: 6,
+        name: 'Accessories'
+      },
+      {
+        id: 7,
+        name: 'Compare'
+      },
+    ]
+  },
+  {
+    id: 4,
+    name: 'Watch',
+    sublinks: [
+      {
+        id: 0,
+        name: 'Apple Watch Series 4'
+      },
+      {
+        id: 1,
+        name: 'Apple Watch Nike+'
+      },
+      {
+        id: 2,
+        name: 'Apple Watch Hermes'
+      },
+      {
+        id: 3,
+        name: 'Apple Watch Series 3'
+      },
+      {
+        id: 4,
+        name: 'watchOS'
+      },
+      {
+        id: 5,
+        name: 'Bands'
+      },
+      {
+        id: 6,
+        name: 'Accessories'
+      },
+      {
+        id: 7,
+        name: 'Compare'
+      },
+    ]
+  },
+  {
+    id: 5,
+    name: 'TV',
+    sublinks: [
+      {
+        id: 0,
+        name: 'Apple TV 4K'
+      },
+      {
+        id: 1,
+        name: 'Apple TV'
+      },
+      {
+        id: 2,
+        name: 'TV App'
+      },
+      {
+        id: 3,
+        name: 'Accessories'
+      },
+      {
+        id: 4,
+        name: 'Compare'
+      },
+    ]
+  },
+  {
+    id: 6,
+    name: 'Music',
+    sublinks: [
+      {
+        id: 0,
+        name: 'Apple Music'
+      },
+      {
+        id: 1,
+        name: 'iTunes'
+      },
+      {
+        id: 2,
+        name: 'HomePod'
+      },
+      {
+        id: 3,
+        name: 'AirPods'
+      },
+      {
+        id: 4,
+        name: 'iPod touch'
+      },
+      {
+        id: 5,
+        name: 'Music Accessories'
+      },
+      {
+        id: 6,
+        name: 'Gift Cards'
+      },
+    ]
+  },
+  {
+    id: 7,
+    name: 'Support',
+    sublinks: []
+  },
+  {
+    id: 8,
+    name: 'Search',
+    sublinks: []
+  },
+  {
+    id: 9,
+    name: 'Shop',
+    sublinks: []
+  },
+]
+
+export default data;
