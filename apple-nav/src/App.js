c0580666ea591ee634f469cc45a691ea5b799177
Yasirah Krueng <ykrueng@MacBook-Pro.local>
2018-11-26 17:38:01 -0600

    add BrowserRouter to App

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,9 +1,15 @@
+// Importing libraries
 import React from 'react';
+
+// Importing data
+import data from './data';
 
 class App extends React.Component {
   render() { 
     return (
-      <div>Hello from App</div>
+      <div className="app">
+        App
+      </div>
     );
   }
 }
