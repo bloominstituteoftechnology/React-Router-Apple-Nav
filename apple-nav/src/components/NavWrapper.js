31e7367f8ed2f2ad48c3d76d10340ae60a5eb84d
wcolts2000 <wcolts2000@gmail.com>
2018-11-26 15:59:14 -0800

    added font awesome icons

--- apple-nav/src/components/NavWrapper.js
+++ apple-nav/src/components/NavWrapper.js
@@ -1,28 +1,61 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const NavigationWrapper = styled.div`
-  display: flex;
-  justify-content: space-around;
   color: #f6f6f6;
   background: #333333;
-  font-size: 14px;
+
+  & > div {
+    font-size: 14px;
+    display: flex;
+    justify-content: space-around;
+    max-width: 1000px;
+    margin: 0 auto;
+
+    & > a {
+      padding: 14px;
+      transition: all 0.2s ease-in;
+    }
+
+    & > a:hover {
+      cursor: pointer;
+      color: #b7b7b7;
+    }
+  }
+`;
+
+const Icon = styled.i`
+  color: #f6f6f6;'
+  font-size: 16px;
+
+  &:hover {
+    cursor: pointer;
+    color: #b7b7b7;
 `;
 
 export default class NavWrapper extends Component {
   render() {
     return (
       <NavigationWrapper>
-        <p>AL</p>
-        <p>Mac</p>
-        <p>iPad</p>
-        <p>iPhone</p>
-        <p>Watch</p>
-        <p>TV</p>
-        <p>Music</p>
-        <p>Support</p>
-        <p>MG</p>
-        <p>SB</p>
+        <div>
+          <Link to="/">
+            <Icon className="fab fa-apple" />
+          </Link>
+          <a>Mac</a>
+          <a>iPad</a>
+          <a>iPhone</a>
+          <a>Watch</a>
+          <a>TV</a>
+          <a>Music</a>
+          <a>Support</a>
+          <Link to="/">
+            <Icon className="fas fa-search" />
+          </Link>
+          <Link to="/">
+            <Icon className="fas fa-shopping-bag" />
+          </Link>
+        </div>
       </NavigationWrapper>
     );
   }
