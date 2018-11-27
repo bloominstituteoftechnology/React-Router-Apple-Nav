97f644a95e1eebd50004cc8ec3af3c397a3becf2
ToMMaaY <thuynh629@gmail.com>
2018-11-27 14:28:54 -0800

    Created a data.js file and added data for Mac category

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 
+import data from "./data";
+
 import "./App.css";
 import TopNav from "./Components/TopNav";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ data: data });
+  }
+
   render() {
     return (
       <div className="App">
---
+++ apple-nav/src/data.js
@@ -0,0 +1,59 @@
+export default [
+  {
+    category: "Mac",
+    products: [
+      {
+        name: "MacBook",
+        imgUrl:
+          "https://www.apple.com/v/mac/home/ae/images/familybrowser/macbook__fjd32xo0xwmm_large.svg"
+      },
+      {
+        name: "MacBook Air",
+        imgUrl:
+          "https://www.apple.com/v/mac/home/ae/images/familybrowser/macbookair__by1vem70tkc2_large.svg",
+        new: "New"
+      },
+      {
+        name: "MacBook Pro",
+        imgUrl:
+          "https://www.apple.com/v/mac/home/ae/images/familybrowser/macbookpro__e0wcugzmt26a_large.svg"
+      },
+      {
+        name: "iMac",
+        imgUrl:
+          "https://www.apple.com/v/mac/home/ae/images/familybrowser/imac__dlz2ciyr6hm6_large.svg"
+      },
+      {
+        name: "iMac Pro",
+        imgUrl:
+          "https://www.apple.com/v/mac/home/ae/images/familybrowser/imac_pro__el8kcar06qky_large.svg"
+      },
+      {
+        name: "Mac Pro",
+        imgUrl:
+          "https://www.apple.com/v/mac/home/ae/images/familybrowser/macpro__dt69qr0ywncm_large.svg"
+      },
+      {
+        name: "Mac mini",
+        imgUrl:
+          "https://www.apple.com/v/mac/home/ae/images/familybrowser/macmini__rzlk7b0xsmqq_large.svg",
+        new: "New"
+      },
+      {
+        name: "Accessories",
+        imgUrl:
+          "https://www.apple.com/v/mac/home/ae/images/familybrowser/mac_acc__fia3ihqup4ae_large.svg"
+      },
+      {
+        name: "Mojave",
+        imgUrl:
+          "https://www.apple.com/v/mac/home/ae/images/familybrowser/mac_osx__dq0x03ehapyu_large.svg"
+      },
+      {
+        name: "Compare",
+        imgUrl:
+          "https://www.apple.com/v/mac/home/ae/images/familybrowser/mac_comp__by57zw2zt32a_large.svg"
+      }
+    ]
+  }
+];
