d82476ff692e93a46c07aa3b18186c76dc40d2d2
ToMMaaY <thuynh629@gmail.com>
2018-11-27 13:45:30 -0800

    Created a React App named apple-nav and set some global CSS styling

---
+++ apple-nav/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
