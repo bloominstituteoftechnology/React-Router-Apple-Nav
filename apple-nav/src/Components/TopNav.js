ea29d9dfcc722335bca95765611e37eb86e1fd1c
ToMMaaY <thuynh629@gmail.com>
2018-11-27 15:24:04 -0800

    Styled the top nav

--- apple-nav/src/Components/TopNav.js
+++ apple-nav/src/Components/TopNav.js
@@ -4,12 +4,20 @@ import { NavLink } from "react-router-dom";
 const TopNav = props => {
   return (
     <div className="top-nav">
-      <NavLink exact to="/">
+      <NavLink exact to="/" className="apple-icon">
         <img src="https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/apple/image_large.svg" />
       </NavLink>
       {props.data.map(item => (
         <NavLink to="/">{item.category}</NavLink>
       ))}
+
+      <NavLink exact to="/" className="search-icon">
+        <img src="https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/search/image_large.svg" />
+      </NavLink>
+
+      <NavLink exact to="/" className="bag-icon">
+        <img src="https://www.apple.com/ac/globalnav/4/en_US/images/globalnav/bag/image_large.svg" />
+      </NavLink>
     </div>
   );
 };
