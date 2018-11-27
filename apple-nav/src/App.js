fc25500dc982440d602382952dbea94b7699a67d
jheinz1999 <jonathan.heinz@aol.com>
2018-11-27 12:52:30 -0800

    set up project

---
+++ apple-nav/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import NavBar from './components/NavBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default App;
