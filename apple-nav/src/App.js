c589fbbd68774032c3dd559b68f635ebdf554442
steelworx <eileencuevas62@gmail.com>
2018-11-27 19:17:52 -0500

    Created SubNavLink.js and refactored SubNavigation.js

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import MainNavigation from './components/MainNavigation';
 import SubNavigation from './components/SubNavigation';
 
+import MacData from './components/Mac/MacData';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      macData: MacData,
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <MainNavigation />
-        <SubNavigation />
-      
+        <Route 
+          exact path='/'
+          render={props => <SubNavigation {...props} data={this.state.macData}/>}
+        />
       </div>
     );
   }
---
+++ apple-nav/src/components/Mac/MacData.js
@@ -0,0 +1,54 @@
+const MacData = [
+    {
+        name: 'MacBook',
+        link: 'macbook',
+        icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/macbook__fjd32xo0xwmm_large.svg',
+    },
+    {
+        name: 'MacBook Air',
+        link: 'macbook-air',
+        icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/macbookair__by1vem70tkc2_large.svg',
+    },
+    {
+        name: 'MacBook Pro',
+        link: 'macbook-pro',
+        icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/macbookpro__e0wcugzmt26a_large.svg',
+    },
+    {
+        name: 'iMac',
+        link: 'imac',
+        icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/imac__dlz2ciyr6hm6_large.svg',
+    },
+    {
+        name: 'iMac Pro',
+        link: 'imac-pro',
+        icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/imac_pro__el8kcar06qky_large.svg',
+    },
+    {
+        name: 'Mac Pro',
+        link: 'mac-pro',
+        icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/macpro__dt69qr0ywncm_large.svg',
+    },
+    {
+        name: 'Mac Mini',
+        link: 'mac-mini',
+        icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/macmini__rzlk7b0xsmqq_large.svg',
+    },
+    {
+        name: 'Accessories',
+        link: 'us/shop/goto/mac/accessories',
+        icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/mac_acc__fia3ihqup4ae_large.svg',
+    },
+    {
+        name: 'Mojave',
+        link: 'macos/mojave',
+        icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/mac_osx__dq0x03ehapyu_large.svg',
+    },
+    {
+        name: 'Compare',
+        link: 'compare',
+        icon: 'https://www.apple.com/v/mac/home/ae/images/familybrowser/mac_comp__by57zw2zt32a_large.svg',
+    },
+]
+
+export default MacData;
