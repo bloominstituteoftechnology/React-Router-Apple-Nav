4c4ee91357ba24b06d51cf504e8df0ef736cea5a
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-11-27 22:38:25 -0500

    Going to Bed I'm done for the Night

--- apple-nav/src/components/SubNav/SubNav.js
+++ apple-nav/src/components/SubNav/SubNav.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 function SubNav(props) {
-  return <h1 />;
+  const apl = apls.find(({ name }) => name === matchMedia.params.name)
+  return 
   //props.apl
   //   .find(a => a.name === props.match.params.name)
-  //   .subLinks.map(apl => (
-  //     <div>
-  //       <img src={apl.img} alt="" />
-  //       {/*<Link to={`/${linkName}/${apl.name}`}>{apl.name}</Link>*/}
-  //     </div>
-  //   ));
+    .subLinks.map(apl => (
+      <div>
+        <img src={apl.img} alt="" />
+        {/*<Link to={`/${linkName}/${apl.name}`}>{apl.name}</Link>*/}
+      </div>
+    ));
 }
 
 export default SubNav;
