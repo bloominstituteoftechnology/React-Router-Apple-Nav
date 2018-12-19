1ea5c312976faad8d50fe2ce10987d788e318069
edreeseg <edward.reeseg@gmail.com>
2018-12-19 12:32:28 -0500

    Removing Content Route 2: Electric Boogaloo.

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import NavBar from './NavBar';
 import SubNav from './SubNav';
-import Content from './Content';
 
 class App extends React.Component {
   render() {
--- apple-nav/src/Content.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Content = props => (
-    <div></div>
-);
-
-export default Content;
