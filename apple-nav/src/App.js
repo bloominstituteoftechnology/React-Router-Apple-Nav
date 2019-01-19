bd4bb877b01a0e4644ea56a76c25b8edbc4eab54
Leila <nb.leila10@gmail.com>
2019-01-19 14:33:35 -0600

    Add animations using react-pose

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import posed, { PoseGroup } from "react-pose";
+import { Route } from 'react-router-dom';
+
 import Nav from './components/Nav';
 import SubNav from './components/SubNav';
-import { Route } from 'react-router-dom';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+//create a posed component with our two visual states, enter and exit.
+const RoutesContainer = posed.div({
+  enter: {  },
+  exit: { }
+});
+
+const App = () => (
+  <Route
+    render={({ location }) => (
       <div className="App">
-        <Route path="/" component={Nav} />
-        <Route path="/:category" component={SubNav} />
+        <PoseGroup>
+          <RoutesContainer key={location.key}>
+            <Route path="/" component={Nav} />
+            <Route path="/:category" component={SubNav} />
+          </RoutesContainer>
+        </PoseGroup>
       </div>
-    );
-  }
-}
+    )}
+  />
+);
 
 export default App;
--- apple-nav/src/components/SubNav.js
+++ apple-nav/src/components/SubNav.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import posed from "react-pose";
+
 import { subCategories } from '../data';
+
+const Item = posed.div({
+  enter: { x: 0, opacity: 1, transition: { ease: 'easeOut', duration: 400 } },
+  exit: { x: 500, opacity: 0 }
+});
 
 const SubNav = (props) => {
   const category = props.match.params.category;
   const subElements = subCategories.find(cat => cat.category === category).sub;
   return ( 
+    <Item>
     <div className="subNav">
-      {subElements.map(sub => <div key={sub}>{sub}</div>)}
+        {subElements.map(sub => <div key={sub}>{sub}</div>)}
     </div>
+    </Item>
    );
 }
  
