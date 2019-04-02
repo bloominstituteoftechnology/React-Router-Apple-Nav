a964ff222e23c866d6559d42b795a07fad40924c
jjgorton <jjgorton@gmail.com>
2019-04-02 19:11:40 -0400

    apple-nav initial setup

---
+++ apple-nav/src/components/NavWrapper.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class NavWrapper extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			navData : []
+		};
+	}
+
+	render() {
+		return (
+			<div className="nav-wrapper">
+				<p>placeholder</p>
+			</div>
+		);
+	}
+}
+
+export default NavWrapper;
