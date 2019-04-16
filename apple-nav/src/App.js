b759dd2bacc7f893a87cb338eeaf49400b511756
david-pok <david.pok@gmail.com>
2019-04-16 16:58:23 -0700

    some boilerplate

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,8 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavWrapper from './Components/NavWrapper';
+import React, { Component } from "react";
+import "./App.css";
+import NavWrapper from "./Components/NavWrapper";
+import SubNav from "./Components/SubNav";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      test: "Thing"
+    };
+  }
+
+  handleClick = test => {
+    this.setState({ test });
+  };
+
   render() {
     return (
       <div className="App">
