eb81a276ef352b924eda9a12bb2dd219efc45ab2
Hargobind Atwal <ethaipath@gmail.com>
2019-05-20 20:44:18 -0700

    displaying data from file

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import data from './data';
-
+console.log(data)
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        {data.macData.map(element => {
+          return <div>{element.name}</div>
+        })}
+        {data.ipadData.map(ele => <div>div</div>)}
       </div>
     );
   }
