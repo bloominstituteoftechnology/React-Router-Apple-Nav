751511b83f5541e758c99da781fb454a2f2ac05c
dannyvidal <vidaldanny94@gmail.com>
2019-07-23 16:11:22 -0400

    added navbar

---
+++ apple-nav/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navigation from "./components/navigation.js";
+import './App.css';
+
+function App() {
+  return (
+    <Navigation />
+  );
+}
+
+export default App;
