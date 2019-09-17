db4d33e0aba2589ba0e7c3751acac95ec2d10f7f
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-09-17 15:46:40 -0500

    fleshed out data and imported to App.js

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { categories } from './Components/Data';
+
 
 function App() {
   return (
     <div className="App">
       <div className='NavBox'>
-        
+
       </div>
     </div>
   );
--- apple-nav/src/Components/Data.js
+++ apple-nav/src/Components/Data.js
@@ -1,7 +1,17 @@
-const categories = [
+export const categories = [
     {
      name: 'Fresh Fruits',
      id: 0,
      subcat:['Golden Delicious', 'McIntosh', 'Gala', 'Fuji', 'Honeycrisp','Pink Lady']  
+    },
+    {
+        name: 'Desserts',
+        id: 1,
+        subcat:['Salted Apple Crisp', 'Perfect Apple Pie', 'Fried Apple Fritters', 'Caramel Apple Poke Cake']
+    },
+    {
+        name: 'Tools',
+        id: 2,
+        subcat:['Doctor Deterrent','Cliche Teacher Gift', 'Overpriced Paperweight', 'Loose Tooth Extractor']
     }
 ]
