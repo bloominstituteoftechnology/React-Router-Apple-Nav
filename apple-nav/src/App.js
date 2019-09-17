298e9937f6604463698abf89dab821c788300bea
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-09-17 16:27:24 -0500

    tweaked Navlinks

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { categories } from './Components/Data';
-
+import MainNav from './Components/MainNav';
 
 function App() {
   return (
     <div className="App">
       <div className='NavBox'>
+    <MainNav />
 
       </div>
     </div>
--- apple-nav/src/Components/SubNav.js
+++ apple-nav/src/Components/SubNav.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 function SubNav() {
     return(
         <div className='sub-nav'>
-
+            
         </div>
     )
 }
